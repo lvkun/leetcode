7ae8a4ec82ad9e9eb9aa3d86b3b8c6ad9b67f6f0
lvkun <lvkun@xiaomi.com>
2017-12-29 18:41:39 +0800

    update median of two sorted arrays

--- src/site/lvkun/median_of_two_sorted_arrays/Solution.java
+++ src/site/lvkun/median_of_two_sorted_arrays/Solution.java
@@ -1,31 +1,28 @@
 package site.lvkun.median_of_two_sorted_arrays;
 
 public class Solution {
-    public double findMedianSortedArrays(int A[], int B[]) {
-        int iA = 0;
-        int iB = 0;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i1 = 0;
+        int i2 = 0;
 
         double last = 0;
         double secondLast = 0;
 
-        while (iA + iB < (A.length + B.length) / 2 + 1) {
+        while (i1 + i2 < (nums1.length + nums2.length) / 2 + 1) {
+            secondLast = last;
 
-            if (iA >= A.length) {
-                secondLast = last;
-                last = B[iB++];
-            } else if (iB >= B.length) {
-                secondLast = last;
-                last = A[iA++];
-            } else if (A[iA] < B[iB]) {
-                secondLast = last;
-                last = A[iA++];
+            if (i1 >= nums1.length) {
+                last = nums2[i2++];
+            } else if (i2 >= nums2.length) {
+                last = nums1[i1++];
+            } else if (nums1[i1] < nums2[i2]) {
+                last = nums1[i1++];
             } else {
-                secondLast = last;
-                last = B[iB++];
+                last = nums2[i2++];
             }
         }
 
-        if ((A.length + B.length) % 2 == 0) {
+        if ((nums1.length + nums2.length) % 2 == 0) {
             return (last + secondLast) / 2;
         }
 
