ed641ac6e0c0ad71e5fb304cb6926a996755f344
lvkun <lvkun@xiaomi.com>
2018-10-18 11:38:26 +0800

    longest palindromic substring.

--- src/site/lvkun/longest_palindromic_substring/Solution.java
+++ src/site/lvkun/longest_palindromic_substring/Solution.java
@@ -1,8 +1,78 @@
 package site.lvkun.longest_palindromic_substring;
 
-public class Solution {
-    public static String longestPalindrome(String s) {
+/*
+Given a string s, find the longest palindromic substring in s.
+You may assume that the maximum length of s is 1000.
 
-        return "";
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+ */
+
+public class Solution {
+    public static class Range {
+        private int mStart;
+        private int mEnd;
+        private String mData;
+
+        public Range(String s, int start, int end) {
+            mData = s;
+            mStart = start;
+            mEnd = end;
+        }
+
+        public static Range create(String s, int start, int end) {
+            Range r = new Range(s, start, end);
+            r.extend();
+            return r;
+        }
+
+        // extend to find largest palindrome
+        private void extend() {
+            while (mStart > 0 && mEnd < mData.length() - 1 && mData.charAt(mStart - 1) == mData.charAt(mEnd + 1)) {
+                mStart -= 1;
+                mEnd += 1;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return mData.substring(mStart, mEnd + 1);
+        }
+
+        public int length() {
+            return mEnd - mStart + 1;
+        }
+    }
+
+    public String longestPalindrome(String s) {
+        Range maxRange = Range.create(s, 0, 0);
+        for (int i = 1; i < s.length() - 1; i++) {
+            // find palindrome around i
+            Range r1 = Range.create(s, i, i);
+
+            if (maxRange.length() < r1.length()) {
+                maxRange = r1;
+            }
+
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                Range r2 = Range.create(s, i, i + 1);
+
+                if (maxRange.length() < r2.length()) {
+                    maxRange = r2;
+                }
+            }
+
+        }
+        return maxRange.toString();
     }
 }
---
+++ src/site/lvkun/longest_palindromic_substring/SolutionTest.java
@@ -0,0 +1,18 @@
+package site.lvkun.longest_palindromic_substring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void testLongestPalindrome() {
+        Solution solution = new Solution();
+
+        Solution.Range range = Solution.Range.create("babad", 1, 1);
+        Assert.assertEquals(range.toString(), "bab");
+
+        Assert.assertEquals("bab", solution.longestPalindrome("babad"));
+        Assert.assertEquals("bb", solution.longestPalindrome("cbbd"));
+    }
+}
