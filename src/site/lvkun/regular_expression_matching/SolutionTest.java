0a9216b5d035f9bf3380255c2ea10c4fe9d5fea0
Bruce Lv <lvkun@outlook.com>
2019-01-12 12:28:50 +0800

    add regular_expression_matching.

---
+++ src/site/lvkun/regular_expression_matching/SolutionTest.java
@@ -0,0 +1,16 @@
+package site.lvkun.regular_expression_matching;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void testMatch() {
+        Solution solution = new Solution();
+        Assert.assertFalse(solution.isMatch("aa", "a"));
+        Assert.assertTrue(solution.isMatch("aa", "a*"));
+        Assert.assertTrue(solution.isMatch("ab", ".*"));
+        Assert.assertTrue(solution.isMatch("aab", "c*a*b"));
+        Assert.assertFalse(solution.isMatch("mississippi", "mis*is*p*."));
+    }
+}
