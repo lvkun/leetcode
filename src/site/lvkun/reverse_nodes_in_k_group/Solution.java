22bd452058f1d1b6ea4d4c7cdad9969c396e9a73
Bruce Lv <lvkun@outlook.com>
2019-01-13 22:26:26 +0800

    solve reverse_nodes_in_k_group.

---
+++ src/site/lvkun/reverse_nodes_in_k_group/Solution.java
@@ -0,0 +1,35 @@
+package site.lvkun.reverse_nodes_in_k_group;
+
+import site.lvkun.common.ListNode;
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode tail = head;
+        int i = 0;
+        while (i < k - 1) {
+            if (tail.next == null) {
+                return head;
+            }
+            tail = tail.next;
+            i++;
+        }
+
+        ListNode nextGroup = tail.next;
+        ListNode newHead = reverse(head, tail);
+        head.next = reverseKGroup(nextGroup, k);
+        return newHead;
+    }
+
+    private ListNode reverse(ListNode head, ListNode tail) {
+        ListNode cur = head;
+        ListNode post = cur.next;
+        while (cur != tail) {
+            ListNode t = post.next;
+            post.next = cur;
+            cur = post;
+            post = t;
+        }
+
+        return tail;
+    }
+}
