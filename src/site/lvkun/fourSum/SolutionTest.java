c529e03253e02588c456087951329bcd92be9afb
Bruce Lv <lvkun@outlook.com>
2019-01-13 16:31:31 +0800

    solve fourSum

--- src/site/lvkun/fourSum/SolutionTest.java
+++ src/site/lvkun/fourSum/SolutionTest.java
@@ -15,7 +15,7 @@ public class SolutionTest {
         Solution solution = new Solution();
         List<List<Integer>> result = solution.fourSum(new int[] {1, 0, -1, 0, -2, 2}, 0);
         Set<List<Integer>> resultSet = new HashSet<>(result);
-        Set<List<Integer>> expectedSet = new HashSet<>(result);
+        Set<List<Integer>> expectedSet = new HashSet<>();
 
         expectedSet.add(Arrays.asList(-1,  0, 0, 1));
         expectedSet.add(Arrays.asList(-2, -1, 1, 2));
