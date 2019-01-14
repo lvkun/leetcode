7b0a615d17ef69695054f076409d230cb698c50b
lvkun <lvkun@xiaomi.com>
2019-01-14 15:30:12 +0800

    add substring_with_concatenation_of_all_words.

---
+++ src/site/lvkun/substring_with_concatenation_of_all_words/Solution.java
@@ -0,0 +1,9 @@
+package site.lvkun.substring_with_concatenation_of_all_words;
+
+import java.util.List;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+}
---
+++ src/site/lvkun/substring_with_concatenation_of_all_words/SolutionTest.java
@@ -0,0 +1,4 @@
+package site.lvkun.substring_with_concatenation_of_all_words;
+
+public class SolutionTest {
+}
