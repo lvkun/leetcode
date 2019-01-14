236b311a6c1594a41bffff89520a29d04ae10ea7
lvkun <lvkun@xiaomi.com>
2019-01-14 17:17:21 +0800

    solve implement_strstr with simple version.

--- src/site/lvkun/implement_strstr/Solution.java
+++ src/site/lvkun/implement_strstr/Solution.java
@@ -2,6 +2,16 @@ package site.lvkun.implement_strstr;
 
 class Solution {
     public int strStr(String haystack, String needle) {
+        for (int i = 0; i < haystack.length(); i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length() - 1) {
+                    return i;
+                }
+            }
+        }
         return -1;
     }
 }
--- src/site/lvkun/implement_strstr/SolutionTest.java
+++ src/site/lvkun/implement_strstr/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.implement_strstr;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testStrStr() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.strStr("hello", "ll"));
+        Assert.assertEquals(-1, solution.strStr("aaaaa", "bba"));
+    }
 }
