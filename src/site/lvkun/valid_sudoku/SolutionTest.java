8931d8a5faf7f2714fd25f9a747625bfdd4e9f49
lvkun <lvkun@xiaomi.com>
2019-01-14 17:17:04 +0800

    fix valid_sudoku.

--- src/site/lvkun/valid_sudoku/SolutionTest.java
+++ src/site/lvkun/valid_sudoku/SolutionTest.java
@@ -1,16 +1,4 @@
 package site.lvkun.valid_sudoku;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class SolutionTest {
-    @Test
-    public void testIsValid() {
-        site.lvkun.valid_parentheses.Solution solution = new site.lvkun.valid_parentheses.Solution();
-        Assert.assertTrue(solution.isValid("()"));
-        Assert.assertTrue(solution.isValid("()[]{}"));
-        Assert.assertFalse(solution.isValid("(]"));
-        Assert.assertFalse(solution.isValid("([)]"));
-        Assert.assertTrue(solution.isValid("{[]}"));
-    }
 }
