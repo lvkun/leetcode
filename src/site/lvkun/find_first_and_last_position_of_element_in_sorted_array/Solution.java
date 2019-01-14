59ce7a7effe383d85e03727efc53533669e9b13c
lvkun <lvkun@xiaomi.com>
2019-01-14 16:15:21 +0800

    add find_first_and_last_position_of_element_in_sorted_array.

---
+++ src/site/lvkun/find_first_and_last_position_of_element_in_sorted_array/Solution.java
@@ -0,0 +1,7 @@
+package site.lvkun.find_first_and_last_position_of_element_in_sorted_array;
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        return null;
+    }
+}
---
+++ src/site/lvkun/find_first_and_last_position_of_element_in_sorted_array/SolutionTest.java
@@ -0,0 +1,4 @@
+package site.lvkun.find_first_and_last_position_of_element_in_sorted_array;
+
+public class SolutionTest {
+}
