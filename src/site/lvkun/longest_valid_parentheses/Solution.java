a55a27001a09d46436ac96cc41200629dc1a0e61
Bruce Lv <lvkun@outlook.com>
2019-01-15 21:44:56 +0800

    solve longest_valid_parentheses.

--- src/site/lvkun/longest_valid_parentheses/Solution.java
+++ src/site/lvkun/longest_valid_parentheses/Solution.java
@@ -1,7 +1,31 @@
 package site.lvkun.longest_valid_parentheses;
 
+import java.util.Stack;
+
 class Solution {
     public int longestValidParentheses(String s) {
-        return 0;
+        Stack<Character> stack = new Stack<>();
+
+        int maxLength = 0;
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.empty()) {
+                    length = 0;
+                } else {
+                    stack.pop();
+                    length += 2;
+                    if (maxLength < length) {
+                        maxLength = length;
+                    }
+                }
+            }
+        }
+
+        return maxLength;
     }
 }
