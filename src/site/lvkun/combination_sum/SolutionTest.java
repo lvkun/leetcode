3173c9136992586b02198a802faee9ba6c5af238
Bruce Lv <lvkun@outlook.com>
2019-01-18 23:15:12 +0800

    solve combination_sum.

--- src/site/lvkun/combination_sum/SolutionTest.java
+++ src/site/lvkun/combination_sum/SolutionTest.java
@@ -1,4 +1,30 @@
 package site.lvkun.combination_sum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+
+    @Test
+    public void testCombinationSum() {
+        Solution solution = new Solution();
+        List<List<Integer>> result1 = solution.combinationSum(array(2,3,6,7), 7);
+
+        Assert.assertEquals(2, result1.size());
+        Assert.assertEquals(Arrays.asList(2,2,3), result1.get(0));
+        Assert.assertEquals(Arrays.asList(7), result1.get(1));
+
+        List<List<Integer>> result2 = solution.combinationSum(array(2,3,5), 8);
+
+        Assert.assertEquals(3, result2.size());
+        Assert.assertEquals(Arrays.asList(2,2,2,2), result2.get(0));
+        Assert.assertEquals(Arrays.asList(2,3,3), result2.get(1));
+        Assert.assertEquals(Arrays.asList(3,5), result2.get(2));
+
+    }
 }
