ee859d86144424ef61c3a0d235c014342f00c9cd
Bruce Lv <lvkun@outlook.com>
2019-01-19 00:03:49 +0800

    solve combination_sum_ii.

--- src/site/lvkun/combination_sum/SolutionTest.java
+++ src/site/lvkun/combination_sum/SolutionTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static site.lvkun.common.ArrayUtils.array;
 
@@ -16,15 +18,17 @@ public class SolutionTest {
         List<List<Integer>> result1 = solution.combinationSum(array(2,3,6,7), 7);
 
         Assert.assertEquals(2, result1.size());
-        Assert.assertEquals(Arrays.asList(2,2,3), result1.get(0));
-        Assert.assertEquals(Arrays.asList(7), result1.get(1));
+        Set<List<Integer>> set1 = new HashSet<>(result1);
+        Assert.assertTrue(set1.contains(Arrays.asList(2,2,3)));
+        Assert.assertTrue(set1.contains(Arrays.asList(7)));
 
         List<List<Integer>> result2 = solution.combinationSum(array(2,3,5), 8);
-
         Assert.assertEquals(3, result2.size());
-        Assert.assertEquals(Arrays.asList(2,2,2,2), result2.get(0));
-        Assert.assertEquals(Arrays.asList(2,3,3), result2.get(1));
-        Assert.assertEquals(Arrays.asList(3,5), result2.get(2));
 
+        Set<List<Integer>> set2 = new HashSet<>(result2);
+
+        Assert.assertTrue(set2.contains(Arrays.asList(2,2,2,2)));
+        Assert.assertTrue(set2.contains(Arrays.asList(2,3,3)));
+        Assert.assertTrue(set2.contains(Arrays.asList(3,5)));
     }
 }
