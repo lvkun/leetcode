7c030260bc75448a043b8c2a6fe115cf20d5f445
Bruce Lv <lvkun@outlook.com>
2019-01-19 22:01:19 +0800

    solve jump_game_ii.

--- src/site/lvkun/jump_game_ii/Solution.java
+++ src/site/lvkun/jump_game_ii/Solution.java
@@ -1,4 +1,25 @@
 package site.lvkun.jump_game_ii;
 
-public class Solution {
+class Solution {
+    private int jump(int[] nums, int start) {
+        if (start == nums.length - 1) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= nums[start]; i++) {
+            if (start + i >= nums.length) {
+                break;
+            }
+
+            int step = 1 + jump(nums, start + i);
+            min = Math.min(min, step);
+        }
+
+        return min;
+    }
+
+    public int jump(int[] nums) {
+        return jump(nums, 0);
+    }
 }
--- src/site/lvkun/jump_game_ii/SolutionTest.java
+++ src/site/lvkun/jump_game_ii/SolutionTest.java
@@ -1,4 +1,14 @@
 package site.lvkun.jump_game_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testJump() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.jump(array(2,3,1,1,4)));
+    }
 }
