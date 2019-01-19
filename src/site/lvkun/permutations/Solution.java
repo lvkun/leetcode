f444734af081251c17547926f41c9d8d39b16076
Bruce Lv <lvkun@outlook.com>
2019-01-19 22:45:00 +0800

    solve permutations.

--- src/site/lvkun/permutations/Solution.java
+++ src/site/lvkun/permutations/Solution.java
@@ -1,4 +1,37 @@
 package site.lvkun.permutations;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+    private void permute(int[] nums, int start, Stack<Integer> result, List<List<Integer>> resultList) {
+        if (start == nums.length) {
+            resultList.add(new ArrayList<>(result));
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            int temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
+
+            result.push(nums[start]);
+            permute(nums, start + 1, result, resultList);
+            result.pop();
+
+            nums[i] = nums[start];
+            nums[start] = temp;
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Stack<Integer> result = new Stack<>();
+
+        permute(nums, 0, result, resultList);
+
+        return resultList;
+    }
 }
