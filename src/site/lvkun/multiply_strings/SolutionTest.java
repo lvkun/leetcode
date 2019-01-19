1ef3568f5c5d123a22d5cd4255474890f353d980
Bruce Lv <lvkun@outlook.com>
2019-01-19 19:40:55 +0800

    solve multiply_strings.

--- src/site/lvkun/multiply_strings/SolutionTest.java
+++ src/site/lvkun/multiply_strings/SolutionTest.java
@@ -1,4 +1,14 @@
 package site.lvkun.multiply_strings;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+
+    @Test
+    public void testMultiply() {
+        Solution solution = new Solution();
+        Assert.assertEquals("6", solution.multiply("3", "2"));
+        Assert.assertEquals("56088", solution.multiply("123", "456"));
+    }
 }
