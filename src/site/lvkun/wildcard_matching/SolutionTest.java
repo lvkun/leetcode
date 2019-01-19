1f6ddb4e310cebc167828acd4f9fd44fdfc2597c
Bruce Lv <lvkun@outlook.com>
2019-01-19 21:40:13 +0800

    solve wildcard_matching.

--- src/site/lvkun/wildcard_matching/SolutionTest.java
+++ src/site/lvkun/wildcard_matching/SolutionTest.java
@@ -1,4 +1,17 @@
 package site.lvkun.wildcard_matching;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testIsMatch() {
+        Solution solution = new Solution();
+        Assert.assertFalse(solution.isMatch("aa", "a"));
+        Assert.assertTrue(solution.isMatch("aa", "*"));
+        Assert.assertTrue(solution.isMatch("", "*"));
+        Assert.assertFalse(solution.isMatch("cb", "?a"));
+        Assert.assertTrue(solution.isMatch("adceb", "*a*b"));
+        Assert.assertFalse(solution.isMatch("acdcb", "a*c?b"));
+    }
 }
