8db49c47b5894819469006873a1f8ec3d5daf285
Bruce Lv <lvkun@outlook.com>
2019-01-19 23:03:24 +0800

    solve permutations_ii.

--- src/site/lvkun/permutations_ii/Solution.java
+++ src/site/lvkun/permutations_ii/Solution.java
@@ -1,4 +1,43 @@
 package site.lvkun.permutations_ii;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    private void permute(int[] nums, int start, Stack<Integer> result, List<List<Integer>> resultList) {
+        if (start == nums.length) {
+            resultList.add(new ArrayList<>(result));
+            return;
+        }
+
+        result.push(nums[start]);
+        permute(nums, start + 1, result, resultList);
+        result.pop();
+        for (int i = start + 1; i < nums.length; i++) {
+            if (nums[i] == nums[start]) {
+                continue;
+            }
+
+            int temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
+
+            result.push(nums[start]);
+            permute(nums, start + 1, result, resultList);
+            result.pop();
+
+            nums[i] = nums[start];
+            nums[start] = temp;
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Stack<Integer> result = new Stack<>();
+
+        permute(nums, 0, result, resultList);
+
+        return resultList;
+    }
 }
--- src/site/lvkun/permutations_ii/SolutionTest.java
+++ src/site/lvkun/permutations_ii/SolutionTest.java
@@ -1,4 +1,21 @@
 package site.lvkun.permutations_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testPermute() {
+        Solution solution = new Solution();
+        List<List<Integer>> resultList = solution.permute(array(1,1,2));
+        Assert.assertEquals(3, resultList.size());
+        Assert.assertEquals(Arrays.asList(1,1,2), resultList.get(0));
+        Assert.assertEquals(Arrays.asList(1,2,1), resultList.get(1));
+        Assert.assertEquals(Arrays.asList(2,1,1), resultList.get(2));
+    }
 }
