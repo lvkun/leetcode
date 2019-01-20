40a46cee61a58c6194dd54833176fbec8015f9f1
Bruce Lv <lvkun@outlook.com>
2019-01-20 21:35:28 +0800

    solve insert_interval.

--- src/site/lvkun/insert_interval/SolutionTest.java
+++ src/site/lvkun/insert_interval/SolutionTest.java
@@ -1,4 +1,25 @@
 package site.lvkun.insert_interval;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.Interval;
+
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testInsert() {
+        Solution solution = new Solution();
+        List<Interval> result = solution.insert(Interval.createList("[[1,3],[6,9]]"),
+                new Interval(2,5));
+        List<Interval> expected = Interval.createList("[[1,5],[6,9]]");
+
+        Assert.assertEquals(expected, result);
+
+        result = solution.insert(Interval.createList("[[1,2],[3,5],[6,7],[8,10],[12,16]]"),
+                new Interval(4, 8));
+        expected = Interval.createList("[[1,2],[3,10],[12,16]]");
+
+        Assert.assertEquals(expected, result);
+    }
 }
