a6f02038ea746a50951a1349696626df1abb03d5
Bruce Lv <lvkun@outlook.com>
2019-01-20 20:23:31 +0800

    solve jump_game.

--- src/site/lvkun/jump_game/Solution.java
+++ src/site/lvkun/jump_game/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.jump_game;
 
-public class Solution {
+class Solution {
+    private boolean canJump(int[] nums, int start) {
+        if (start == nums.length - 1) {
+            return true;
+        }
+
+        int steps = nums[start];
+        if (steps == 0) {
+            return false;
+        }
+
+        for (int i = 1; i <= steps; i++) {
+            if (canJump(nums, start + i)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        return canJump(nums, 0);
+    }
 }
--- src/site/lvkun/jump_game/SolutionTest.java
+++ src/site/lvkun/jump_game/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.jump_game;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testCanJump() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.canJump(array(2,3,1,1,4)));
+        Assert.assertFalse(solution.canJump(array(3,2,1,0,4)));
+    }
 }
