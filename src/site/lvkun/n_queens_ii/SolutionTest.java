5d0c3b362df54cd8f55f4a333ba81695915f70ca
Bruce Lv <lvkun@outlook.com>
2019-01-20 18:18:30 +0800

    solve n_queens_ii.

--- src/site/lvkun/n_queens_ii/SolutionTest.java
+++ src/site/lvkun/n_queens_ii/SolutionTest.java
@@ -1,4 +1,12 @@
 package site.lvkun.n_queens_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testTotalNQueens() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.totalNQueens(4));
+    }
 }
