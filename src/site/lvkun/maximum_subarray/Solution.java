c362dfcd984216b0eb156c1a3dcb58b90b993698
Bruce Lv <lvkun@outlook.com>
2019-01-20 18:25:13 +0800

    solve maximum_subarray.

--- src/site/lvkun/maximum_subarray/Solution.java
+++ src/site/lvkun/maximum_subarray/Solution.java
@@ -1,4 +1,21 @@
 package site.lvkun.maximum_subarray;
 
-public class Solution {
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum <= 0) {
+                sum = 0;
+                continue;
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+
+        return maxSum;
+    }
 }
