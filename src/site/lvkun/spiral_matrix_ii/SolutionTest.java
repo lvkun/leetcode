e7337e2d7886c9ab7872316259e8c066b239becb
Bruce Lv <lvkun@outlook.com>
2019-01-20 22:35:15 +0800

    solve spiral_matrix_ii.

--- src/site/lvkun/spiral_matrix_ii/SolutionTest.java
+++ src/site/lvkun/spiral_matrix_ii/SolutionTest.java
@@ -1,4 +1,18 @@
 package site.lvkun.spiral_matrix_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testGenerateMatrix() {
+        Solution solution = new Solution();
+        int[][] result = solution.generateMatrix(3);
+
+        Assert.assertArrayEquals(array(1, 2, 3), result[0]);
+        Assert.assertArrayEquals(array(8, 9, 4), result[1]);
+        Assert.assertArrayEquals(array(7, 6, 5), result[2]);
+    }
 }
