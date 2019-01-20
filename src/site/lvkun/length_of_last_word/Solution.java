613d187fdfb54861082acda4ef7c0ddb726a8be7
Bruce Lv <lvkun@outlook.com>
2019-01-20 22:08:04 +0800

    solve length_of_last_word.

--- src/site/lvkun/length_of_last_word/Solution.java
+++ src/site/lvkun/length_of_last_word/Solution.java
@@ -1,4 +1,14 @@
 package site.lvkun.length_of_last_word;
 
-public class Solution {
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int lastSpaceIndex = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                lastSpaceIndex = i;
+            }
+        }
+
+        return s.length() - lastSpaceIndex - 1;
+    }
 }
