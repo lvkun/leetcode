b4075bc3d2d3d177205f7df2a2213410c0f7111c
Bruce Lv <lvkun@outlook.com>
2019-01-20 23:23:16 +0800

    solve permutation_sequence.

--- src/site/lvkun/permutation_sequence/Solution.java
+++ src/site/lvkun/permutation_sequence/Solution.java
@@ -1,4 +1,45 @@
 package site.lvkun.permutation_sequence;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        if (n == 0) {
+            return "";
+        }
+
+        if (n == 1) {
+            return "1";
+        }
+
+        int[] factor = new int[n];
+        factor[0] = 1;
+        for (int i = 1; i < n; i++) {
+            factor[i] = factor[i - 1] * i;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        List<Integer> candidates = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            candidates.add(i);
+        }
+
+        k--;
+        for (int i = 0; i < n; i++) {
+            int f = factor[n - i - 1];
+            int index = k / f;
+            k = k % f;
+
+            result.add(candidates.get(index));
+            candidates.remove(index);
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (Integer integer : result) {
+            builder.append(integer);
+        }
+
+        return builder.toString();
+    }
 }
--- src/site/lvkun/permutation_sequence/SolutionTest.java
+++ src/site/lvkun/permutation_sequence/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.permutation_sequence;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testPermutation() {
+        Solution solution = new Solution();
+        Assert.assertEquals("213", solution.getPermutation(3, 3));
+        Assert.assertEquals("2314", solution.getPermutation(4, 9));
+    }
 }
