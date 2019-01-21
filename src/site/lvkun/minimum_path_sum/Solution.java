dd0cc056b8b6e4b3ef1b52f3f29d9390171e7220
Bruce Lv <lvkun@outlook.com>
2019-01-21 23:35:34 +0800

    solve minimum_path_sum.

--- src/site/lvkun/minimum_path_sum/Solution.java
+++ src/site/lvkun/minimum_path_sum/Solution.java
@@ -1,4 +1,34 @@
 package site.lvkun.minimum_path_sum;
 
-public class Solution {
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >=0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    continue;
+                }
+
+                if (i == m - 1) {
+                    grid[i][j] += grid[i][j + 1];
+                    continue;
+                }
+
+                if (j == n - 1) {
+                    grid[i][j] += grid[i + 1][j];
+                    continue;
+                }
+
+                grid[i][j] += Math.min(grid[i][j + 1], grid[i + 1][j]);
+            }
+        }
+
+        return grid[0][0];
+    }
 }
