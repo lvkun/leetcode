05f9eb399778e2f04b4f1a83e84c18a8abcc7940
Bruce Lv <lvkun@outlook.com>
2019-01-21 20:12:36 +0800

    solve unique_paths.

--- src/site/lvkun/unique_paths/Solution.java
+++ src/site/lvkun/unique_paths/Solution.java
@@ -1,4 +1,14 @@
 package site.lvkun.unique_paths;
 
-public class Solution {
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] result = new int[n];
+        result[0] = 1;
+        for (int row = 0; row < m; row++) {
+            for (int col= 1; col < n; col++) {
+                result[col] += result[col - 1];
+            }
+        }
+        return result[n - 1];
+    }
 }
--- src/site/lvkun/unique_paths/SolutionTest.java
+++ src/site/lvkun/unique_paths/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.unique_paths;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testUniquePaths() {
+        Solution solution = new Solution();
+        Assert.assertEquals(3, solution.uniquePaths(3, 2));
+        Assert.assertEquals(28, solution.uniquePaths(7, 3));
+    }
 }
