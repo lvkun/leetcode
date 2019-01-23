a9a314a9271cb3110a49652eb162c40acf7e2507
Bruce Lv <lvkun@outlook.com>
2019-01-23 22:19:34 +0800

    solve simplify_path.

--- src/site/lvkun/simplify_path/Solution.java
+++ src/site/lvkun/simplify_path/Solution.java
@@ -1,4 +1,24 @@
 package site.lvkun.simplify_path;
 
-public class Solution {
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> pathList = new Stack<>();
+        for (String dir : path.split("/")) {
+            if (dir.contentEquals(".") || dir.length() == 0) {
+                continue;
+            }
+
+            if (dir.contentEquals("..")) {
+                if (!pathList.isEmpty()) {
+                    pathList.pop();
+                }
+            } else {
+                pathList.push(dir);
+            }
+        }
+
+        return "/" + String.join("/", pathList);
+    }
 }
--- src/site/lvkun/simplify_path/SolutionTest.java
+++ src/site/lvkun/simplify_path/SolutionTest.java
@@ -1,4 +1,17 @@
 package site.lvkun.simplify_path;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testSimplifyPath() {
+        Solution solution = new Solution();
+        Assert.assertEquals("/home", solution.simplifyPath("/home/"));
+        Assert.assertEquals("/", solution.simplifyPath("/../"));
+        Assert.assertEquals("/home/foo", solution.simplifyPath("/home//foo/"));
+        Assert.assertEquals("/c", solution.simplifyPath("/a/./b/../../c/"));
+        Assert.assertEquals("/c", solution.simplifyPath("/a/../../b/../c//.//"));
+        Assert.assertEquals("/a/b/c", solution.simplifyPath("/a//b////c/d//././/.."));
+    }
 }
