fc09eed905f5c7fcf87a4847525c474074d74b4e
Bruce Lv <lvkun@outlook.com>
2019-01-23 21:24:49 +0800

    solve sqrtx.

--- src/site/lvkun/sqrtx/SolutionTest.java
+++ src/site/lvkun/sqrtx/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.sqrtx;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testSqrtX() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.mySqrt(4));
+        Assert.assertEquals(2, solution.mySqrt(8));
+    }
 }
