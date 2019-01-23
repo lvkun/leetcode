c8938dd7070b0ea4642b46957d2ba2616dfa5dbc
Bruce Lv <lvkun@outlook.com>
2019-01-23 21:29:36 +0800

    solve climbing_stairs.

--- src/site/lvkun/climbing_stairs/SolutionTest.java
+++ src/site/lvkun/climbing_stairs/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.climbing_stairs;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testClimbStairs() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.climbStairs(2));
+        Assert.assertEquals(3, solution.climbStairs(3));
+    }
 }
