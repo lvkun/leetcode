a7ad2b1a7a778c40938ae606228d9898ecbaae2e
Bruce Lv <lvkun@outlook.com>
2019-01-24 23:06:09 +0800

    solve search_a_2d_matrix.

--- src/site/lvkun/search_a_2d_matrix/Solution.java
+++ src/site/lvkun/search_a_2d_matrix/Solution.java
@@ -1,4 +1,34 @@
 package site.lvkun.search_a_2d_matrix;
 
-public class Solution {
+class Solution {
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int start = 0;
+        int end = n * m - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if (matrix[row][col] < target) {
+                start = mid + 1;
+            } else if (matrix[row][col] > target) {
+                end = mid - 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
