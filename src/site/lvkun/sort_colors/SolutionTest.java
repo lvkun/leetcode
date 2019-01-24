c425632379a787029cf39d9de7222dbff445102d
Bruce Lv <lvkun@outlook.com>
2019-01-24 23:46:35 +0800

    solve sort_colors.

--- src/site/lvkun/sort_colors/SolutionTest.java
+++ src/site/lvkun/sort_colors/SolutionTest.java
@@ -1,4 +1,18 @@
 package site.lvkun.sort_colors;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testSortColors() {
+        Solution solution = new Solution();
+        int[] result = array(2,0,2,1,1,0);
+        solution.sortColors(result);
+
+        Assert.assertArrayEquals(array(0,0,1,1,2,2),
+                result);
+    }
 }
