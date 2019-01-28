ae2d11c16b2addbbff6e939d0c54c7e9b44e4216
Bruce Lv <lvkun@outlook.com>
2019-01-28 21:41:43 +0800

    solve word_search.

--- src/site/lvkun/word_search/SolutionTest.java
+++ src/site/lvkun/word_search/SolutionTest.java
@@ -1,4 +1,22 @@
 package site.lvkun.word_search;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.matrixChar;
+
 public class SolutionTest {
+
+    @Test
+    public void testExist() {
+        Solution solution = new Solution();
+        char[][] board = matrixChar("[\n" +
+                "  ['A','B','C','E'],\n" +
+                "  ['S','F','C','S'],\n" +
+                "  ['A','D','E','E']\n" +
+                "]\n");
+        Assert.assertTrue(solution.exist(board, "ABCCED"));
+        Assert.assertTrue(solution.exist(board, "SEE"));
+        Assert.assertFalse(solution.exist(board, "ABCB"));
+    }
 }
