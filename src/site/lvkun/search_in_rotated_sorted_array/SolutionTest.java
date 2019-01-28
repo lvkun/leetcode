b35c7d4caa03e3b03e2787b6546b8e0d6a03540f
Bruce Lv <lvkun@outlook.com>
2019-01-29 00:13:39 +0800

    solve search_in_rotated_sorted_array_ii.

--- src/site/lvkun/search_in_rotated_sorted_array/SolutionTest.java
+++ src/site/lvkun/search_in_rotated_sorted_array/SolutionTest.java
@@ -7,10 +7,10 @@ public class SolutionTest {
     @Test
     public void testSearch() {
         Solution solution = new Solution();
-//        Assert.assertEquals(4,
-//                solution.search(new int[]{4,5,6,7,0,1,2}, 0));
-//        Assert.assertEquals(-1,
-//                solution.search(new int[]{4,5,6,7,0,1,2}, 3));
+        Assert.assertEquals(4,
+                solution.search(new int[]{4,5,6,7,0,1,2}, 0));
+        Assert.assertEquals(-1,
+                solution.search(new int[]{4,5,6,7,0,1,2}, 3));
         Assert.assertEquals(2,
                 solution.search(new int[]{5,1,3}, 3));
     }
--- src/site/lvkun/search_in_rotated_sorted_array_ii/Solution.java
+++ src/site/lvkun/search_in_rotated_sorted_array_ii/Solution.java
@@ -1,4 +1,33 @@
 package site.lvkun.search_in_rotated_sorted_array_ii;
 
-public class Solution {
+class Solution {
+    private boolean mayInRange(int[] nums, int start, int end, int target) {
+        if (nums[start] < nums[end]) {
+            return nums[start] < target && target < nums[end];
+        } else {
+            return target > nums[start] || target < nums[end];
+        }
+    }
+
+    public boolean search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (mayInRange(nums, start, mid, target)) {
+                end = mid - 1;
+            } else if (mayInRange(nums, mid, end, target)) {
+                start = mid + 1;
+            } else {
+                end--;
+            }
+        }
+
+        return false;
+    }
 }
