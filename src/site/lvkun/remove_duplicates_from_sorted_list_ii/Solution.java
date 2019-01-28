90bd7cdcf4e10dcb394d2607f72e92b9fdad42b1
Bruce Lv <lvkun@outlook.com>
2019-01-29 01:00:45 +0800

    solve remove_duplicates_from_sorted_list_ii.

--- src/site/lvkun/remove_duplicates_from_sorted_list_ii/Solution.java
+++ src/site/lvkun/remove_duplicates_from_sorted_list_ii/Solution.java
@@ -1,4 +1,33 @@
 package site.lvkun.remove_duplicates_from_sorted_list_ii;
 
-public class Solution {
+import site.lvkun.common.ListNode;
+
+class Solution {
+    private boolean equals(ListNode n1, ListNode n2) {
+        if (n1 == null || n2 == null) {
+            return false;
+        }
+
+        return n1.val == n2.val;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode list = dummy;
+        ListNode prev = dummy;
+        ListNode next = prev.next;
+        while (next != null) {
+            if (!equals(next, next.next) && !equals(prev, next)) {
+                list.next = next;
+                list = next;
+            }
+
+            prev = next;
+            next = next.next;
+        }
+
+        return dummy.next;
+    }
 }
--- src/site/lvkun/remove_duplicates_from_sorted_list_ii/SolutionTest.java
+++ src/site/lvkun/remove_duplicates_from_sorted_list_ii/SolutionTest.java
@@ -1,4 +1,19 @@
 package site.lvkun.remove_duplicates_from_sorted_list_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.ListNode;
+
 public class SolutionTest {
+    @Test
+    public void testDeleteDuplicates() {
+        Solution solution = new Solution();
+        ListNode node = ListNode.fromString("1->2->3->3->4->4->5");
+        ListNode result = solution.deleteDuplicates(node);
+        Assert.assertEquals("1->2->5", result.toString());
+
+        node = ListNode.fromString("1->1->1->2->3");
+        result = solution.deleteDuplicates(node);
+        Assert.assertEquals("2->3", result.toString());
+    }
 }
