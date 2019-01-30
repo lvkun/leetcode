3164696cd493b8e16410315ae784b2ecd3309fd6
Bruce Lv <lvkun@outlook.com>
2019-01-30 21:11:35 +0800

    solve largest_rectangle_in_histogram.

--- src/site/lvkun/largest_rectangle_in_histogram/SolutionTest.java
+++ src/site/lvkun/largest_rectangle_in_histogram/SolutionTest.java
@@ -1,4 +1,18 @@
 package site.lvkun.largest_rectangle_in_histogram;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testLargestRectangleArea() {
+        Solution solution = new Solution();
+        Assert.assertEquals(10,
+                solution.largestRectangleArea(array(2,1,5,6,2,3)));
+
+        Assert.assertEquals(3,
+                solution.largestRectangleArea(array(2,0,2,1,1)));
+    }
 }
