e78de8465b3d63d51c24d6da5b530ba9f5e3b7b2
Bruce Lv <lvkun@outlook.com>
2019-02-01 21:55:28 +0800

    solve scramble_string.

--- src/site/lvkun/scramble_string/Solution.java
+++ src/site/lvkun/scramble_string/Solution.java
@@ -1,4 +1,35 @@
 package site.lvkun.scramble_string;
 
-public class Solution {
+// TODO: retry
+class Solution {
+    public boolean isScramble(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() != s2.length()) {
+            return false;
+        }
+
+        int n = s1.length();
+        if (n == 0) {
+            return false;
+        }
+
+        boolean[][][] dp = new boolean[n][n][n + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j][1] = s1.charAt(i) == s2.charAt(j);
+            }
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                for (int j = 0; j < n - len + 1; j++) {
+                    for (int k = 1; k < len; k++) {
+                        dp[i][j][len] |= dp[i][j][k] && dp[i + k][j + k][len - k]
+                                || dp[i][j + len - k][k]&&dp[i + k][j][len - k];
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][n];
+    }
 }
--- src/site/lvkun/scramble_string/SolutionTest.java
+++ src/site/lvkun/scramble_string/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.scramble_string;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testIsScramble() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.isScramble("great", "rgeat"));
+        Assert.assertFalse(solution.isScramble("abcde", "caebd"));
+    }
 }
