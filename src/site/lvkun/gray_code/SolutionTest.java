37ceacb57c9fbdb251870adf0d54e14911729fd4
Bruce Lv <lvkun@outlook.com>
2019-02-01 23:25:11 +0800

    solve gray_code.

--- src/site/lvkun/gray_code/SolutionTest.java
+++ src/site/lvkun/gray_code/SolutionTest.java
@@ -1,4 +1,19 @@
 package site.lvkun.gray_code;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testGrayCode() {
+        Solution solution = new Solution();
+        List<Integer> result = solution.grayCode(2);
+        Assert.assertEquals(Arrays.asList(0,1,3,2), result);
+
+        result = solution.grayCode(0);
+        Assert.assertEquals(Arrays.asList(0), result);
+    }
 }
