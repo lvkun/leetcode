f27756edd2ce23ac284eaa4494ac6751edc0115c
Bruce Lv <lvkun@outlook.com>
2019-02-01 22:12:07 +0800

    solve merge_sorted_array.

--- src/site/lvkun/merge_sorted_array/SolutionTest.java
+++ src/site/lvkun/merge_sorted_array/SolutionTest.java
@@ -1,4 +1,16 @@
 package site.lvkun.merge_sorted_array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testMerge() {
+        Solution solution = new Solution();
+        int[] result = array(1,2,3,0,0,0);
+        solution.merge(result, 3, array(2,5,6), 3);
+        Assert.assertArrayEquals(array(1,2,2,3,5,6), result);
+    }
 }
