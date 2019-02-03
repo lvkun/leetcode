f9a3411120e78c9f2efca3a966f53da41e99b843
Bruce Lv <lvkun@outlook.com>
2019-02-03 15:16:34 +0800

    solve subsets_ii.

--- src/site/lvkun/subsets_ii/Solution.java
+++ src/site/lvkun/subsets_ii/Solution.java
@@ -1,4 +1,35 @@
 package site.lvkun.subsets_ii;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        resultList.add(new ArrayList<>(result));
+
+        if (nums == null || nums.length == 0) {
+            return resultList;
+        }
+
+        result.add(nums[0]);
+        resultList.add(new ArrayList<>(result));
+        int size = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                size = resultList.size();
+            }
+            int newSize = resultList.size();
+            for (int j = newSize - size; j < newSize; j++) {
+                result = new ArrayList<>(resultList.get(j));
+                result.add(nums[i]);
+                resultList.add(result);
+            }
+        }
+
+        return resultList;
+    }
 }
--- src/site/lvkun/subsets_ii/SolutionTest.java
+++ src/site/lvkun/subsets_ii/SolutionTest.java
@@ -1,4 +1,31 @@
 package site.lvkun.subsets_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static site.lvkun.common.ArrayUtils.array;
+import static site.lvkun.common.ArrayUtils.from;
+
 public class SolutionTest {
+    @Test
+    public void testSubsetsWithDup() {
+        Solution solution = new Solution();
+        List<List<Integer>> resultList
+                = solution.subsetsWithDup(array(1,2,2));
+        List<List<Integer>> expectedList = from("[\n" +
+                "  [2],\n" +
+                "  [1],\n" +
+                "  [1,2,2],\n" +
+                "  [2,2],\n" +
+                "  [1,2],\n" +
+                "  []\n" +
+                "]");
+        Set<List<Integer>> resultSet = new HashSet<>(resultList);
+        Set<List<Integer>> expectedSet = new HashSet<>(expectedList);
+        Assert.assertEquals(expectedSet, resultSet);
+    }
 }
