409c39a78c7249fe0a9e50d37a4f6c4a8f4460e7
Bruce Lv <lvkun@outlook.com>
2019-02-09 21:57:53 +0800

    solve reverse_linked_list_ii.

--- src/site/lvkun/reverse_linked_list_ii/SolutionTest.java
+++ src/site/lvkun/reverse_linked_list_ii/SolutionTest.java
@@ -1,4 +1,16 @@
 package site.lvkun.reverse_linked_list_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.ListNode;
+
 public class SolutionTest {
+    @Test
+    public void testReverseBetween() {
+        Solution solution = new Solution();
+
+        ListNode head = ListNode.fromString("1->2->3->4->5");
+        ListNode result = solution.reverseBetween(head, 2, 4);
+        Assert.assertEquals("1->4->3->2->5", result.toString());
+    }
 }
