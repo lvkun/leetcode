3c7634fa22b8c11b27199231bae2160ba3d30152
Bruce Lv <lvkun@outlook.com>
2019-02-13 21:02:09 +0800

    fix decode_ways.

--- src/site/lvkun/decode_ways/Solution.java
+++ src/site/lvkun/decode_ways/Solution.java
@@ -7,16 +7,17 @@ class Solution {
         }
 
         int[] dp = new int[s.length() + 1];
-        for (int i = 1; i < s.length(); i++) {
+        dp[0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
             if (s.charAt(i - 1) == '0') {
                 dp[i] = 0;
             } else {
-                dp[i] = dp[i - 1];
+                dp[i] += dp[i - 1];
             }
 
             if (i > 1
-                    && s.charAt(i - 2) == '2'
-                    && s.charAt(i - 2) == '1'
+                    && (s.charAt(i - 2) == '2'
+                    || s.charAt(i - 2) == '1')
                     && s.charAt(i - 1) <= '6') {
                 dp[i] += dp[i - 2];
             }
