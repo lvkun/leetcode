a8b7dca1f2f088b2c44b5a19868dd5cda87c3d29
Bruce Lv <lvkun@outlook.com>
2019-02-15 00:19:17 +0800

    solve unique_binary_search_trees.

--- src/site/lvkun/unique_binary_search_trees/SolutionTest.java
+++ src/site/lvkun/unique_binary_search_trees/SolutionTest.java
@@ -1,4 +1,12 @@
 package site.lvkun.unique_binary_search_trees;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testNumTrees() {
+        Solution solution = new Solution();
+        Assert.assertEquals(5, solution.numTrees(3));
+    }
 }
