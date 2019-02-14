d73b4f689d48aa382589432ee4897fbe2395ccbe
Bruce Lv <lvkun@outlook.com>
2019-02-14 21:08:59 +0800

    solve binary_tree_inorder_traversal.

--- src/site/lvkun/binary_tree_inorder_traversal/SolutionTest.java
+++ src/site/lvkun/binary_tree_inorder_traversal/SolutionTest.java
@@ -1,4 +1,22 @@
 package site.lvkun.binary_tree_inorder_traversal;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
+import java.util.Arrays;
+import java.util.List;
+
+// TODO: retry
 public class SolutionTest {
+    @Test
+    public void testInorderTraversal() {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        Solution solution = new Solution();
+        List<Integer> result = solution.inorderTraversal(root);
+        Assert.assertEquals(Arrays.asList(1, 3, 2), result);
+    }
 }
---
+++ src/site/lvkun/common/TreeNode.java
@@ -0,0 +1,8 @@
+package site.lvkun.common;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
