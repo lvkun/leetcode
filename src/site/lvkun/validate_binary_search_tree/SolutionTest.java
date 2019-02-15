572828fd9bdaba77098b9dd910927b776b12d53c
Bruce Lv <lvkun@outlook.com>
2019-02-15 23:44:40 +0800

    solve validate_binary_search_tree.

--- src/site/lvkun/validate_binary_search_tree/SolutionTest.java
+++ src/site/lvkun/validate_binary_search_tree/SolutionTest.java
@@ -1,4 +1,26 @@
 package site.lvkun.validate_binary_search_tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
 public class SolutionTest {
+    @Test
+    public void testIsValidBST() {
+        Solution solution = new Solution();
+
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+
+        Assert.assertTrue(solution.isValidBST(root));
+
+        root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(6);
+
+        Assert.assertFalse(solution.isValidBST(root));
+    }
 }
