5b895b62ae6dcf6bf9a97b627ec822733795c042
Bruce Lv <lvkun@outlook.com>
2019-02-16 21:46:32 +0800

    solve maximum_depth_of_binary_tree.

--- src/site/lvkun/maximum_depth_of_binary_tree/Solution.java
+++ src/site/lvkun/maximum_depth_of_binary_tree/Solution.java
@@ -1,4 +1,13 @@
 package site.lvkun.maximum_depth_of_binary_tree;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+    }
 }
