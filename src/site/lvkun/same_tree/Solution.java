1f5e798dee5bd54732605cfbea7c8159c149cf02
Bruce Lv <lvkun@outlook.com>
2019-02-16 19:20:47 +0800

    solve same_tree.

--- src/site/lvkun/same_tree/Solution.java
+++ src/site/lvkun/same_tree/Solution.java
@@ -1,4 +1,18 @@
 package site.lvkun.same_tree;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left)
+                    && isSameTree(p.right, q.right);
+        }
+
+        return false;
+    }
 }
