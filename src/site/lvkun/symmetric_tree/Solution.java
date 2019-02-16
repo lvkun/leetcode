f2e43e903d29b7d6a47eb1d8becbbba398778ccc
Bruce Lv <lvkun@outlook.com>
2019-02-16 20:08:55 +0800

    solve symmetric_tree.

--- src/site/lvkun/symmetric_tree/Solution.java
+++ src/site/lvkun/symmetric_tree/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.symmetric_tree;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left != null && right != null) {
+            return isSymmetric(left.left, right.right)
+                    && isSymmetric(left.right, right.left);
+        }
+
+        return false;
+    }
 }
--- src/site/lvkun/symmetric_tree/SolutionTest.java
+++ src/site/lvkun/symmetric_tree/SolutionTest.java
@@ -1,4 +1,31 @@
 package site.lvkun.symmetric_tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
 public class SolutionTest {
+    @Test
+    public void testIsSymmetric() {
+        Solution solution = new Solution();
+        TreeNode root;
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        Assert.assertTrue(solution.isSymmetric(root));
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+
+        Assert.assertFalse(solution.isSymmetric(root));
+    }
 }
