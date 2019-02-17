65a74a71db93df0f7ecd5c95b733ca142f9d922f
Bruce Lv <lvkun@outlook.com>
2019-02-17 23:23:40 +0800

    solve triangle.

--- src/site/lvkun/triangle/Solution.java
+++ src/site/lvkun/triangle/Solution.java
@@ -1,4 +1,22 @@
 package site.lvkun.triangle;
 
-public class Solution {
+import java.util.List;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        List<Integer> lastRow = triangle.get(triangle.size() - 1);
+        int[] row = new int[lastRow.size()];
+        for (int i = 0; i < lastRow.size(); i++) {
+            row[i] = lastRow.get(i);
+        }
+
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            List<Integer> curRow = triangle.get(i);
+            for (int j = 0; j < curRow.size(); j++) {
+                row[j] = Math.min(row[j], row[j + 1]) + curRow.get(j);
+            }
+        }
+
+        return row[0];
+    }
 }
