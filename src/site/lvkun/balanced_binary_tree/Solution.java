494ada0ddf391b67ce1117f3befb97d09f5fad8c
Bruce Lv <lvkun@outlook.com>
2019-02-17 13:27:34 +0800

    solve balanced_binary_tree.

--- src/site/lvkun/balanced_binary_tree/Solution.java
+++ src/site/lvkun/balanced_binary_tree/Solution.java
@@ -1,4 +1,31 @@
 package site.lvkun.balanced_binary_tree;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkDepth(root) != -1;
+    }
+
+    private int checkDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = checkDepth(node.left);
+        if (left == -1) {
+            return -1;
+        }
+
+        int right = checkDepth(node.right);
+        if (right == -1) {
+            return -1;
+        }
+
+        if (Math.abs(right - left) <= 1) {
+            return 1 + Math.max(right, left);
+        }
+
+        return -1;
+    }
 }
--- src/site/lvkun/balanced_binary_tree/SolutionTest.java
+++ src/site/lvkun/balanced_binary_tree/SolutionTest.java
@@ -1,4 +1,32 @@
 package site.lvkun.balanced_binary_tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
 public class SolutionTest {
+    @Test
+    public void testIsBalanced() {
+        Solution solution = new Solution();
+
+        TreeNode root;
+
+        root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Assert.assertTrue(solution.isBalanced(root));
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.left.left.left = new TreeNode(4);
+        root.left.left.right = new TreeNode(4);
+        root.right = new TreeNode(2);
+
+        Assert.assertFalse(solution.isBalanced(root));
+    }
 }
