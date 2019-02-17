b01703ad7882a80a909a20ff6cc76652c9c9f0da
Bruce Lv <lvkun@outlook.com>
2019-02-17 20:39:44 +0800

    solve pascals_triangle.

--- src/site/lvkun/pascals_triangle/SolutionTest.java
+++ src/site/lvkun/pascals_triangle/SolutionTest.java
@@ -1,4 +1,23 @@
 package site.lvkun.pascals_triangle;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testGenerate() {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.generate(5);
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1),
+                Arrays.asList(1, 1),
+                Arrays.asList(1, 2, 1),
+                Arrays.asList(1, 3, 3, 1),
+                Arrays.asList(1, 4, 6, 4, 1)
+        );
+        Assert.assertEquals(expected, result);
+    }
 }
