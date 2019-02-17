e920730c635ca470ac0d6db678151e81addcef78
Bruce Lv <lvkun@outlook.com>
2019-02-17 12:39:43 +0800

    solve convert_sorted_list_to_binary_search_tree.

--- src/site/lvkun/convert_sorted_list_to_binary_search_tree/Solution.java
+++ src/site/lvkun/convert_sorted_list_to_binary_search_tree/Solution.java
@@ -1,4 +1,34 @@
 package site.lvkun.convert_sorted_list_to_binary_search_tree;
 
-public class Solution {
+import site.lvkun.common.ListNode;
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        return sortedListToBST(head, null);
+    }
+
+    private TreeNode sortedListToBST(ListNode head, ListNode tail) {
+        if (head == tail) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != tail && fast.next != tail) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        TreeNode node = new TreeNode(slow.val);
+        node.left = sortedListToBST(head, slow);
+        node.right = sortedListToBST(slow.next, tail);
+
+        return node;
+    }
 }
