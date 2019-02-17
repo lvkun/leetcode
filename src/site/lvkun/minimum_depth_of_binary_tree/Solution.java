fe82d877588512856b19ae9e993582f70e491f2f
Bruce Lv <lvkun@outlook.com>
2019-02-17 14:56:39 +0800

    solve minimum_depth_of_binary_tree.

--- src/site/lvkun/minimum_depth_of_binary_tree/Solution.java
+++ src/site/lvkun/minimum_depth_of_binary_tree/Solution.java
@@ -1,4 +1,13 @@
 package site.lvkun.minimum_depth_of_binary_tree;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
 }
