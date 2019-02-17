3c28059fd7cf396f806a41eb7e27bd77275d1659
Bruce Lv <lvkun@outlook.com>
2019-02-17 18:24:27 +0800

    solve populating_next_right_pointers_in_each_node.

--- src/site/lvkun/populating_next_right_pointers_in_each_node/Solution.java
+++ src/site/lvkun/populating_next_right_pointers_in_each_node/Solution.java
@@ -1,4 +1,34 @@
 package site.lvkun.populating_next_right_pointers_in_each_node;
 
-public class Solution {
+import site.lvkun.common.Node;
+
+// TODO: retry
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node start = root;
+
+        while (start.left != null) {
+            Node cur = start;
+
+            while (cur != null) {
+                cur.left.next = cur.right;
+
+                if (cur.next != null) {
+                    cur.right.next = cur.next.left;
+                }
+
+                cur = cur.next;
+            }
+
+
+            start = start.left;
+        }
+
+
+        return root;
+    }
 }
