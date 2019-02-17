2d9ff5d42a2c1e0a4cfa9296185dd4e9d23f1c8e
Bruce Lv <lvkun@outlook.com>
2019-02-17 15:04:10 +0800

    solve path_sum.

--- src/site/lvkun/path_sum/Solution.java
+++ src/site/lvkun/path_sum/Solution.java
@@ -1,4 +1,14 @@
 package site.lvkun.path_sum;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return sum == 0;
+        }
+
+        int newSum = sum - root.val;
+        return hasPathSum(root.left, newSum) || hasPathSum(root.right, newSum);
+    }
 }
--- src/site/lvkun/path_sum/SolutionTest.java
+++ src/site/lvkun/path_sum/SolutionTest.java
@@ -1,4 +1,24 @@
 package site.lvkun.path_sum;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
 public class SolutionTest {
+    @Test
+    public void testHasPathSum() {
+        Solution solution = new Solution();
+
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+
+        Assert.assertTrue(solution.hasPathSum(root, 22));
+    }
 }
