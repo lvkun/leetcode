c6eece10450a3c8b655bd2e9f2c18afb687f44ad
Bruce Lv <lvkun@outlook.com>
2019-02-17 23:39:26 +0800

    solve best_time_to_buy_and_sell_stock.

--- src/site/lvkun/best_time_to_buy_and_sell_stock/Solution.java
+++ src/site/lvkun/best_time_to_buy_and_sell_stock/Solution.java
@@ -1,4 +1,14 @@
 package site.lvkun.best_time_to_buy_and_sell_stock;
 
-public class Solution {
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            min = Math.min(min, price);
+            profit = Math.max(profit, price - min);
+        }
+
+        return profit;
+    }
 }
