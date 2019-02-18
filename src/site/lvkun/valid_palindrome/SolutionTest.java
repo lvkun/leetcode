a46bfdcc149caffe1256754a01fc0cec581f314d
Bruce Lv <lvkun@outlook.com>
2019-02-18 23:58:38 +0800

    solve valid_palindrome.

--- src/site/lvkun/valid_palindrome/SolutionTest.java
+++ src/site/lvkun/valid_palindrome/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.valid_palindrome;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testIsPalindrome() {
+        Solution solution = new Solution();
+        Assert.assertTrue(
+                solution.isPalindrome("A man, a plan, a canal: Panama"));
+        Assert.assertFalse(
+                solution.isPalindrome("race a car"));
+    }
 }
