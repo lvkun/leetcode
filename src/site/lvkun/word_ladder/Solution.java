01af3fc1b19fc85cfdac8a1aeeb9385594ace176
Bruce Lv <lvkun@outlook.com>
2019-02-19 21:28:54 +0800

    solve word_ladder.

--- src/site/lvkun/word_ladder/Solution.java
+++ src/site/lvkun/word_ladder/Solution.java
@@ -1,4 +1,52 @@
 package site.lvkun.word_ladder;
 
-public class Solution {
+import java.util.*;
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Map<String, Boolean> wordMap = new HashMap<String, Boolean>();
+        for (String word : wordList) {
+            wordMap.put(word, false);
+        }
+
+        Queue<String> queue = new ArrayDeque<>();
+        queue.offer(beginWord);
+
+        Queue<String> next = new ArrayDeque<>();
+
+        int count = 0;
+        while (queue.size() > 0) {
+            for (String word : queue) {
+                if (word.compareTo(endWord) == 0) {
+                    return count + 1;
+                }
+
+                for (int i = 0; i < word.length(); i++) {
+                    char[] arr = word.toCharArray();
+                    char origin_char = arr[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        arr[i] = c;
+
+                        String newWord = new String(arr);
+                        boolean hasVisited = wordMap.getOrDefault(newWord, true);
+                        if (!hasVisited) {
+                            next.add(newWord);
+                            wordMap.put(newWord, true);
+                        }
+                    }
+
+                    arr[i] = origin_char;
+                }
+            }
+
+            queue.clear();
+            Queue<String> temp = queue;
+            queue = next;
+            next = temp;
+
+            count++;
+        }
+
+        return 0;
+    }
 }
--- src/site/lvkun/word_ladder/SolutionTest.java
+++ src/site/lvkun/word_ladder/SolutionTest.java
@@ -1,4 +1,21 @@
 package site.lvkun.word_ladder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class SolutionTest {
+    @Test
+    public void testLadderLength() {
+        Solution solution = new Solution();
+        int result = solution.ladderLength("hit", "cog",
+                Arrays.asList("hot","dot","dog","lot","log","cog"));
+        Assert.assertEquals(5, result);
+
+        result = solution.ladderLength("hit", "cog",
+                Arrays.asList("hot","dot","dog","lot","log"));
+
+        Assert.assertEquals(0, result);
+    }
 }
