0f1bbf11a7d24a58323c47ae3e29b5c4f56c6661
Bruce Lv <lvkun@outlook.com>
2019-02-19 22:38:08 +0800

    solve course_schedule.

--- src/site/lvkun/course_schedule/SolutionTest.java
+++ src/site/lvkun/course_schedule/SolutionTest.java
@@ -1,4 +1,18 @@
 package site.lvkun.course_schedule;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.matrixInt;
+
 public class SolutionTest {
+    @Test
+    public void testCanFinish() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.canFinish(2, matrixInt("[[1, 0]]")));
+        Assert.assertFalse(solution.canFinish(2, matrixInt("[" +
+                "[1, 0],\n" +
+                "[0, 1]" +
+                "]")));
+    }
 }
