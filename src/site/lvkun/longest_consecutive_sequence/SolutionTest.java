565b53cad86e1012304d6dcee76f8332c6893493
Bruce Lv <lvkun@outlook.com>
2019-02-19 21:58:00 +0800

    solve longest_consecutive_sequence.

--- src/site/lvkun/longest_consecutive_sequence/SolutionTest.java
+++ src/site/lvkun/longest_consecutive_sequence/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.longest_consecutive_sequence;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testLongestConsecutive() {
+        Solution solution = new Solution();
+        Assert.assertEquals(4,
+                solution.longestConsecutive(array(100, 4, 200, 1, 3, 2)));
+    }
 }
