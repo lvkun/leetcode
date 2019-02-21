f91bfe68ba96701be7d22249df8c7804b0a17aaf
Bruce Lv <lvkun@outlook.com>
2019-02-21 11:08:03 +0800

    solve single_number_ii.

--- src/site/lvkun/single_number_ii/Solution.java
+++ src/site/lvkun/single_number_ii/Solution.java
@@ -1,4 +1,19 @@
 package site.lvkun.single_number_ii;
 
-public class Solution {
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                sum += (nums[j] >> i) & 1;
+            }
+
+            res |= (sum % 3) << i;
+        }
+
+        return res;
+    }
 }
