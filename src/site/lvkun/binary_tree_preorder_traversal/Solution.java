e6a712735077b6357648621a6e3a43062201b4a4
Bruce Lv <lvkun@outlook.com>
2019-02-21 19:52:22 +0800

    solve binary_tree_preorder_traversal.

--- src/site/lvkun/binary_tree_preorder_traversal/Solution.java
+++ src/site/lvkun/binary_tree_preorder_traversal/Solution.java
@@ -1,4 +1,31 @@
 package site.lvkun.binary_tree_preorder_traversal;
 
-public class Solution {
+import site.lvkun.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        return result;
+    }
 }
--- src/site/lvkun/binary_tree_preorder_traversal/SolutionTest.java
+++ src/site/lvkun/binary_tree_preorder_traversal/SolutionTest.java
@@ -1,4 +1,20 @@
 package site.lvkun.binary_tree_preorder_traversal;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testPreorderTraversal() {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        List<Integer> result  = solution.preorderTraversal(root);
+        Assert.assertEquals(Arrays.asList(1,2,3), result);
+    }
 }
