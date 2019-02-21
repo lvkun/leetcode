6440c699607ce3abef217607b9f986626031cb9d
Bruce Lv <lvkun@outlook.com>
2019-02-21 10:35:11 +0800

    solve single_number.

--- src/site/lvkun/single_number/Solution.java
+++ src/site/lvkun/single_number/Solution.java
@@ -1,4 +1,13 @@
 package site.lvkun.single_number;
 
-public class Solution {
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i <nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
 }
