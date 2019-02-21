4ad278b4f2a16ff3008a7a18ad595c3646837877
Bruce Lv <lvkun@outlook.com>
2019-02-21 21:26:27 +0800

    solve insertion_sort_list.

--- src/site/lvkun/insertion_sort_list/Solution.java
+++ src/site/lvkun/insertion_sort_list/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.insertion_sort_list;
 
-public class Solution {
+import site.lvkun.common.ListNode;
+
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+
+        while (head != null) {
+            ListNode prev = dummy;
+
+            while (prev.next != null && prev.next.val < head.val) {
+                prev = prev.next;
+            }
+
+            ListNode next = head.next;
+
+            head.next = prev.next;
+            prev.next = head;
+
+            head = next;
+        }
+
+        return dummy.next;
+    }
 }
