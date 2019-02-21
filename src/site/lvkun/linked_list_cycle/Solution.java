e2415a8530e90dfde6d5967f6e434e6edd14145d
Bruce Lv <lvkun@outlook.com>
2019-02-21 12:45:38 +0800

    solve linked_list_cycle.

--- src/site/lvkun/linked_list_cycle/Solution.java
+++ src/site/lvkun/linked_list_cycle/Solution.java
@@ -1,4 +1,21 @@
 package site.lvkun.linked_list_cycle;
 
+import site.lvkun.common.ListNode;
+
 public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/site/lvkun/linked_list_cycle/SolutionTest.java
+++ src/site/lvkun/linked_list_cycle/SolutionTest.java
@@ -1,4 +1,21 @@
 package site.lvkun.linked_list_cycle;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.ListNode;
+
 public class SolutionTest {
+    @Test
+    public void testHasCycle() {
+        Solution solution = new Solution();
+
+        ListNode head = ListNode.fromString("3->2->0->4");
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = head.next;
+
+        Assert.assertTrue(solution.hasCycle(head));
+    }
 }
