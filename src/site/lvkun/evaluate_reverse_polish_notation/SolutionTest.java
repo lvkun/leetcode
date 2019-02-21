dc89fec24f74550ed420dada101b926e9925dff4
Bruce Lv <lvkun@outlook.com>
2019-02-21 23:52:09 +0800

    solve evaluate_reverse_polish_notation.

--- src/site/lvkun/evaluate_reverse_polish_notation/SolutionTest.java
+++ src/site/lvkun/evaluate_reverse_polish_notation/SolutionTest.java
@@ -1,4 +1,16 @@
 package site.lvkun.evaluate_reverse_polish_notation;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testEvalRPN() {
+        Solution solution = new Solution();
+        Assert.assertEquals(9, solution.evalRPN(array("2", "1", "+", "3", "*")));
+        Assert.assertEquals(6, solution.evalRPN(array("4", "13", "5", "/", "+")));
+        Assert.assertEquals(22, solution.evalRPN(array("10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+")));
+    }
 }
