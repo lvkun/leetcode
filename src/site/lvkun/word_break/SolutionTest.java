399d495bffd1b9c390e25a8449411dac340cbb77
Bruce Lv <lvkun@outlook.com>
2019-02-21 12:38:32 +0800

    solve word_break.

--- src/site/lvkun/word_break/SolutionTest.java
+++ src/site/lvkun/word_break/SolutionTest.java
@@ -1,4 +1,16 @@
 package site.lvkun.word_break;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class SolutionTest {
+    @Test
+    public void testWordBreak() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.wordBreak("leetcode", Arrays.asList("leet", "code")));
+        Assert.assertTrue(solution.wordBreak("applepenapple", Arrays.asList("apple", "pen")));
+        Assert.assertFalse(solution.wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+    }
 }
