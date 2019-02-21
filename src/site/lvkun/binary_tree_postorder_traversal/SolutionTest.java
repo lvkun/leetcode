e432d4375946fc2cae470dc46b848bf5ef785b1a
Bruce Lv <lvkun@outlook.com>
2019-02-21 20:15:17 +0800

    solve binary_tree_postorder_traversal.

--- src/site/lvkun/binary_tree_postorder_traversal/SolutionTest.java
+++ src/site/lvkun/binary_tree_postorder_traversal/SolutionTest.java
@@ -1,4 +1,20 @@
 package site.lvkun.binary_tree_postorder_traversal;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.common.TreeNode;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testPostorderTraversal() {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        List<Integer> result  = solution.postorderTraversal(root);
+        Assert.assertEquals(Arrays.asList(3,2,1), result);
+    }
 }
