5c43b932206c8629b428d3aec6e1c3459ea8b9dc
Bruce Lv <lvkun@outlook.com>
2019-02-21 23:37:56 +0800

    solve sort_list.

--- src/site/lvkun/sort_list/Solution.java
+++ src/site/lvkun/sort_list/Solution.java
@@ -1,4 +1,50 @@
 package site.lvkun.sort_list;
 
-public class Solution {
+import site.lvkun.common.ListNode;
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode fast = head;
+        ListNode prev = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        prev.next = null;
+
+        return merge(sortList(head), sortList(slow));
+    }
+
+    private ListNode merge(ListNode first, ListNode second) {
+        ListNode dummy = new ListNode(-1);
+
+        ListNode result = dummy;
+
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                result.next = first;
+                first = first.next;
+            } else {
+                result.next = second;
+                second = second.next;
+            }
+            result = result.next;
+        }
+
+        if (first != null) {
+            result.next = first;
+        } else if (second != null) {
+            result.next = second;
+        }
+
+        return dummy.next;
+    }
+
 }
