dff3ea232b637e6470a0e13b6b260a087a02b625
Bruce Lv <lvkun@outlook.com>
2019-02-23 17:29:10 +0800

    solve maximum_product_subarray.

--- src/site/lvkun/maximum_product_subarray/Solution.java
+++ src/site/lvkun/maximum_product_subarray/Solution.java
@@ -1,4 +1,24 @@
 package site.lvkun.maximum_product_subarray;
 
-public class Solution {
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+
+        max[0] = nums[0];
+        min[0] = nums[0];
+
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max[i] = Math.max(Math.max(max[i - 1] * nums[i], min[i - 1] * nums[i]), nums[i]);
+            min[i] = Math.min(Math.min(max[i - 1] * nums[i], min[i - 1] * nums[i]), nums[i]);
+            res = Math.max(res, max[i]);
+        }
+
+        return res;
+    }
 }
