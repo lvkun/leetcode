10a0df3cc6b5d9dbdd3dfba13abfa0a32edf47a4
Bruce Lv <lvkun@outlook.com>
2019-02-25 21:38:15 +0800

    solve find_minimum_in_rotated_sorted_array_ii.

--- src/site/lvkun/find_minimum_in_rotated_sorted_array_ii/SolutionTest.java
+++ src/site/lvkun/find_minimum_in_rotated_sorted_array_ii/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.find_minimum_in_rotated_sorted_array_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testFindMin() {
+        Solution solution = new Solution();
+        Assert.assertEquals(1, solution.findMin(array(1,3,5)));
+        Assert.assertEquals(0, solution.findMin(array(2,2,2,0,1)));
+    }
 }
