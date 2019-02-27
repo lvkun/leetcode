1df4e32943d916233323b40e9072c9c12819cb5a
Bruce Lv <lvkun@outlook.com>
2019-02-27 10:40:37 +0800

    solve find_peak_element.

--- src/site/lvkun/find_peak_element/Solution.java
+++ src/site/lvkun/find_peak_element/Solution.java
@@ -1,4 +1,18 @@
 package site.lvkun.find_peak_element;
 
-public class Solution {
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return end;
+    }
 }
--- src/site/lvkun/find_peak_element/SolutionTest.java
+++ src/site/lvkun/find_peak_element/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.find_peak_element;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testFindPeakElement() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.findPeakElement(array(1,2,3,1)));
+        Assert.assertEquals(5, solution.findPeakElement(array(1,2,1,3,5,6,4)));
+    }
 }
