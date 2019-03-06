997d691ade590370a556b423749a8583d660e98b
Bruce Lv <lvkun@outlook.com>
2019-03-06 22:00:15 +0800

    solve two_sum_ii_input_array_is_sorted.

--- src/site/lvkun/two_sum_ii_input_array_is_sorted/Solution.java
+++ src/site/lvkun/two_sum_ii_input_array_is_sorted/Solution.java
@@ -1,4 +1,21 @@
 package site.lvkun.two_sum_ii_input_array_is_sorted;
 
-public class Solution {
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start != end) {
+            int sum = numbers[start] + numbers[end];
+            if (sum < target) {
+                start++;
+            } else if (sum > target) {
+                end--;
+            } else {
+                return new int[] {start + 1, end + 1};
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
 }
--- src/site/lvkun/two_sum_ii_input_array_is_sorted/SolutionTest.java
+++ src/site/lvkun/two_sum_ii_input_array_is_sorted/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.two_sum_ii_input_array_is_sorted;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testTwoSum() {
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(array(2,7,11,15), 9);
+        Assert.assertArrayEquals(array(1, 2), result);
+    }
 }
