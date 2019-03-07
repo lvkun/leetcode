46465225e0636d3565bd43fde4d5a88ed180d83f
Bruce Lv <lvkun@outlook.com>
2019-03-07 21:47:03 +0800

    solve majority_element.

--- src/site/lvkun/majority_element/SolutionTest.java
+++ src/site/lvkun/majority_element/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.majority_element;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testMajorityElement() {
+        Solution solution = new Solution();
+        Assert.assertEquals(3, solution.majorityElement(array(3,2,3)));
+        Assert.assertEquals(2, solution.majorityElement(array(2,2,1,1,1,2,2)));
+    }
 }
