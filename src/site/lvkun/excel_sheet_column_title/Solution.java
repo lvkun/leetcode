e173f2d1d8a9db46ce67591d1b3debb690d9791a
Bruce Lv <lvkun@outlook.com>
2019-03-07 21:08:15 +0800

    solve excel_sheet_column_title.

--- src/site/lvkun/excel_sheet_column_title/Solution.java
+++ src/site/lvkun/excel_sheet_column_title/Solution.java
@@ -1,4 +1,33 @@
 package site.lvkun.excel_sheet_column_title;
 
-public class Solution {
+import java.util.Stack;
+
+class Solution {
+
+    private char getChar(int i) {
+        return (char) ('A' + i - 1);
+    }
+
+    public String convertToTitle(int n) {
+        Stack<Character> result = new Stack<>();
+
+        while (n > 0) {
+            int r = n % 26;
+
+            if (r == 0) {
+                result.push('Z');
+                n -= 26;
+            } else {
+                result.push(getChar(r));
+            }
+
+            n = n / 26;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!result.isEmpty()) {
+            sb.append(result.pop());
+        }
+        return sb.toString();
+    }
 }
