489f1d6228ccc878ca258b2fc4002b18dbba54da
Bruce Lv <lvkun@outlook.com>
2019-03-07 23:22:52 +0800

    solve excel_sheet_column_number.

--- src/site/lvkun/excel_sheet_column_number/Solution.java
+++ src/site/lvkun/excel_sheet_column_number/Solution.java
@@ -1,4 +1,22 @@
 package site.lvkun.excel_sheet_column_number;
 
-public class Solution {
+class Solution {
+    private int fromChar(char c) {
+        return c - 'A' + 1;
+    }
+
+    public int titleToNumber(String s) {
+        char[] charArray = s.toCharArray();
+
+        int result = 0;
+
+        for (int i = 0; i < charArray.length; i++) {
+            int v = fromChar(charArray[i]);
+
+            result = 26 * result + v;
+        }
+
+
+        return result;
+    }
 }
--- src/site/lvkun/excel_sheet_column_number/SolutionTest.java
+++ src/site/lvkun/excel_sheet_column_number/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.excel_sheet_column_number;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testTitleToNumber() {
+        Solution solution = new Solution();
+
+        Assert.assertEquals(1, solution.titleToNumber("A"));
+        Assert.assertEquals(28, solution.titleToNumber("AB"));
+        Assert.assertEquals(701, solution.titleToNumber("ZY"));
+    }
 }
