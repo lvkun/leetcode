68b503cdfd76456f12befafda0e48b40cbb36c46
Bruce Lv <lvkun@outlook.com>
2019-03-08 23:33:44 +0800

    solve factorial_trailing_zeroes.

--- src/site/lvkun/factorial_trailing_zeroes/Solution.java
+++ src/site/lvkun/factorial_trailing_zeroes/Solution.java
@@ -1,4 +1,14 @@
 package site.lvkun.factorial_trailing_zeroes;
 
-public class Solution {
+class Solution {
+    public int trailingZeroes(int n) {
+        int res = 0;
+
+        while (n > 0) {
+            res += n / 5;
+            n /= 5;
+        }
+
+        return res;
+    }
 }
--- src/site/lvkun/factorial_trailing_zeroes/SolutionTest.java
+++ src/site/lvkun/factorial_trailing_zeroes/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.factorial_trailing_zeroes;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void tesTrailingZeroes() {
+        Solution solution = new Solution();
+        Assert.assertEquals(0, solution.trailingZeroes(3));
+        Assert.assertEquals(1, solution.trailingZeroes(5));
+    }
 }
