80abec229492b88fb2846c94231ef5aea289151f
Bruce Lv <lvkun@outlook.com>
2019-03-11 00:22:43 +0800

    solve repeated_dna_sequences.

--- src/site/lvkun/leetcode/repeated_dna_sequences/Solution.java
+++ src/site/lvkun/leetcode/repeated_dna_sequences/Solution.java
@@ -1,4 +1,37 @@
 package site.lvkun.leetcode.repeated_dna_sequences;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> result = new HashSet<>();
+
+        if (s == null || s.length() < 10) {
+            return new ArrayList<>(result);
+        }
+
+        char[] array = s.toCharArray();
+        int key = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            char c = array[i];
+            key = ((key << 3) | c & 7);
+        }
+
+        for (int i = 9; i < s.length(); i++) {
+            char c = array[i];
+            key = (((0x7ffffff & key) << 3) | (c & 7));
+
+            if (set.contains(key)) {
+                result.add(new String(array, i - 9, 10));
+            }
+            set.add(key);
+        }
+
+        return new ArrayList<>(result);
+    }
 }
--- src/site/lvkun/leetcode/repeated_dna_sequences/SolutionTest.java
+++ src/site/lvkun/leetcode/repeated_dna_sequences/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.leetcode.repeated_dna_sequences;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class SolutionTest {
+    @Test
+    public void testFindRepeatedDnaSequences() {
+        Solution solution = new Solution();
+        Assert.assertEquals(Arrays.asList("AAAAACCCCC", "CCCCCAAAAA"),
+                solution.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+    }
 }
