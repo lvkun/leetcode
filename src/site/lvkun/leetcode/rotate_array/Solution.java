217a038b01541ac22ca7ec82b6f41799fdc71afd
Bruce Lv <lvkun@outlook.com>
2019-03-11 00:46:27 +0800

    solve rotate_array.

--- src/site/lvkun/leetcode/rotate_array/Solution.java
+++ src/site/lvkun/leetcode/rotate_array/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.leetcode.rotate_array;
 
-public class Solution {
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int i = 0;
+        int turn = 0;
+        int start = 0;
+        int newNum = nums[i];
+        int oldNum;
+        while (turn < nums.length) {
+            i = (i + k) % nums.length;
+
+            oldNum = nums[i];
+            nums[i] = newNum;
+            newNum = oldNum;
+
+            if (i == start) {
+                start++;
+                i++;
+
+                newNum = nums[i];
+            }
+            turn++;
+        }
+    }
 }
--- src/site/lvkun/leetcode/rotate_array/SolutionTest.java
+++ src/site/lvkun/leetcode/rotate_array/SolutionTest.java
@@ -1,4 +1,20 @@
 package site.lvkun.leetcode.rotate_array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testRotate() {
+        Solution solution = new Solution();
+        int[] nums = array(1,2,3,4,5,6,7);
+        solution.rotate(nums, 3);
+        Assert.assertArrayEquals(array(5,6,7,1,2,3,4), nums);
+
+        nums = array(-1,-100,3,99);
+        solution.rotate(nums, 2);
+        Assert.assertArrayEquals(array(3,99,-1,-100), nums);
+    }
 }
