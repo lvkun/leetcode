05789d66679583913714495c2a4d71ec6f9ea08a
Bruce Lv <lvkun@outlook.com>
2019-03-11 01:14:42 +0800

    solve number_of_1_bits.

--- src/site/lvkun/leetcode/number_of_1_bits/Solution.java
+++ src/site/lvkun/leetcode/number_of_1_bits/Solution.java
@@ -1,4 +1,15 @@
 package site.lvkun.leetcode.number_of_1_bits;
 
 public class Solution {
+    public int hammingWeight(int n) {
+        int mask = 1;
+        int count = 0;
+        while (n != 0) {
+            if ((n & mask) != 0) {
+                count++;
+            }
+            n >>>= 1; // must use unsigned right shift operator;
+        }
+        return count;
+    }
 }
--- src/site/lvkun/leetcode/number_of_1_bits/SolutionTest.java
+++ src/site/lvkun/leetcode/number_of_1_bits/SolutionTest.java
@@ -1,4 +1,14 @@
 package site.lvkun.leetcode.number_of_1_bits;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testHammingWeight() {
+        Solution solution = new Solution();
+        Assert.assertEquals(3, solution.hammingWeight(Integer.valueOf("00000000000000000000000000001011", 2)));
+        Assert.assertEquals(1, solution.hammingWeight(Integer.valueOf("00000000000000000000000010000000", 2)));
+        Assert.assertEquals(31, solution.hammingWeight(-3));
+    }
 }
