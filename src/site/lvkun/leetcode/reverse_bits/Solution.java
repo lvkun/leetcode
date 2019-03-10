c1310580d1bb70cdacb0e76d3cc602c5adf613ed
Bruce Lv <lvkun@outlook.com>
2019-03-11 01:06:01 +0800

    solve reverse_bits.

--- src/site/lvkun/leetcode/reverse_bits/Solution.java
+++ src/site/lvkun/leetcode/reverse_bits/Solution.java
@@ -1,4 +1,30 @@
 package site.lvkun.leetcode.reverse_bits;
 
 public class Solution {
+    private boolean getBit(int n, int bit) {
+        int masked = 1 << bit;
+        return (n & masked) != 0;
+    }
+
+    private int setBit(int n, int bit, boolean v) {
+        if (v) {
+            n |= 1 << bit;
+        } else {
+            n &= ~(1 << bit);
+        }
+
+        return n;
+    }
+
+    public int reverseBits(int n) {
+        for (int i = 0; i < 15; i++) {
+            int j = 31 - i;
+            boolean vi = getBit(n, i);
+            boolean vj = getBit(n, j);
+            n = setBit(n, i, vj);
+            n = setBit(n, j, vi);
+        }
+
+        return n;
+    }
 }
--- src/site/lvkun/leetcode/reverse_bits/SolutionTest.java
+++ src/site/lvkun/leetcode/reverse_bits/SolutionTest.java
@@ -1,4 +1,12 @@
 package site.lvkun.leetcode.reverse_bits;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testReverseBits() {
+        Solution solution = new Solution();
+        Assert.assertEquals((int)Integer.valueOf("00111001011110000010100101000000", 2), solution.reverseBits(Integer.valueOf("00000010100101000001111010011100", 2)));
+    }
 }
