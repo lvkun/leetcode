00ccb73cfea99a24c2ad644dd2936c21485ca37f
Bruce Lv <lvkun@outlook.com>
2019-03-11 01:25:33 +0800

    solve house_robber.

--- src/site/lvkun/leetcode/house_robber/Solution.java
+++ src/site/lvkun/leetcode/house_robber/Solution.java
@@ -1,4 +1,31 @@
 package site.lvkun.leetcode.house_robber;
 
-public class Solution {
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int index = 0;
+
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        while (index < nums.length - 1) {
+            sumOdd += nums[index];
+            sumEven += nums[index + 1];
+
+            index += 2;
+        }
+
+        if (index == nums.length - 1) {
+            sumOdd += nums[index];
+        }
+
+        return Math.max(sumOdd, sumEven);
+    }
 }
--- src/site/lvkun/leetcode/house_robber/SolutionTest.java
+++ src/site/lvkun/leetcode/house_robber/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.leetcode.house_robber;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testRob() {
+        Solution solution = new Solution();
+        Assert.assertEquals(4, solution.rob(array(1,2,3,1)));
+        Assert.assertEquals(12, solution.rob(array(2,7,9,3,1)));
+    }
 }
