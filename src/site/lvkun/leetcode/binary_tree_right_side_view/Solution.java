4d6e5f9f3662bb7b61b7afaebce0e5c16565f6ac
Bruce Lv <lvkun@outlook.com>
2019-03-11 21:42:39 +0800

    solve binary_tree_right_side_view.

--- src/site/lvkun/leetcode/binary_tree_right_side_view/Solution.java
+++ src/site/lvkun/leetcode/binary_tree_right_side_view/Solution.java
@@ -1,4 +1,46 @@
 package site.lvkun.leetcode.binary_tree_right_side_view;
 
-public class Solution {
+import site.lvkun.leetcode.common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        Queue<TreeNode> nextLevel = new ArrayDeque<>();
+
+        while (!queue.isEmpty()) {
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                if (queue.isEmpty()) {
+                    result.add(node.val);
+                }
+
+                if (node.left != null) {
+                    nextLevel.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    nextLevel.offer(node.right);
+                }
+            }
+
+            queue = nextLevel;
+            nextLevel = new ArrayDeque<>();
+        }
+
+
+        return result;
+    }
 }
--- src/site/lvkun/leetcode/binary_tree_right_side_view/SolutionTest.java
+++ src/site/lvkun/leetcode/binary_tree_right_side_view/SolutionTest.java
@@ -1,4 +1,23 @@
 package site.lvkun.leetcode.binary_tree_right_side_view;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.leetcode.common.TreeNode;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testRightSideView() {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+
+        List<Integer> result = solution.rightSideView(root);
+        Assert.assertEquals(Arrays.asList(1, 3, 4), result);
+    }
 }
