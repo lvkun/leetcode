9944d176d5c0860842254214b08160d215013e09
Bruce Lv <lvkun@outlook.com>
2019-03-11 23:14:17 +0800

    solve bitwise_and_of_numbers_range.

--- src/site/lvkun/leetcode/bitwise_and_of_numbers_range/SolutionTest.java
+++ src/site/lvkun/leetcode/bitwise_and_of_numbers_range/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.leetcode.bitwise_and_of_numbers_range;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        Assert.assertEquals(4, solution.rangeBitwiseAnd(5, 7));
+        Assert.assertEquals(0, solution.rangeBitwiseAnd(0, 1));
+    }
 }
