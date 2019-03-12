f12cd58c650b0b473eb399c7e0ac45a2db4039b8
Bruce Lv <lvkun@outlook.com>
2019-03-12 21:53:02 +0800

    solve happy_number.

--- src/site/lvkun/leetcode/happy_number/Solution.java
+++ src/site/lvkun/leetcode/happy_number/Solution.java
@@ -1,4 +1,25 @@
 package site.lvkun.leetcode.happy_number;
 
-public class Solution {
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (!set.contains(n)) {
+            set.add(n);
+
+            int sum = 0;
+            while (n != 0) {
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            n = sum;
+            if (n == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/site/lvkun/leetcode/happy_number/SolutionTest.java
+++ src/site/lvkun/leetcode/happy_number/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.leetcode.happy_number;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testIsHappy() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.isHappy(19));
+        Assert.assertFalse(solution.isHappy(11));
+    }
 }
