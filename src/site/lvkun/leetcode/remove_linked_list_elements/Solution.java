2aaac4727016bc8f65b9fd8037e678030748ef58
Bruce Lv <lvkun@outlook.com>
2019-03-12 22:11:08 +0800

    solve remove_linked_list_elements.

--- src/site/lvkun/leetcode/remove_linked_list_elements/Solution.java
+++ src/site/lvkun/leetcode/remove_linked_list_elements/Solution.java
@@ -1,4 +1,23 @@
 package site.lvkun.leetcode.remove_linked_list_elements;
 
-public class Solution {
+import site.lvkun.leetcode.common.ListNode;
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+
+        }
+
+        return dummyHead.next;
+    }
 }
--- src/site/lvkun/leetcode/remove_linked_list_elements/SolutionTest.java
+++ src/site/lvkun/leetcode/remove_linked_list_elements/SolutionTest.java
@@ -1,4 +1,14 @@
 package site.lvkun.leetcode.remove_linked_list_elements;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.leetcode.common.ListNode;
+
 public class SolutionTest {
+    @Test
+    public void testRemoveElements() {
+        Solution solution = new Solution();
+        ListNode result = solution.removeElements(ListNode.fromString("1->2->6->3->4->5->6"), 6);
+        Assert.assertEquals("1->2->3->4->5", result.toString());
+    }
 }
