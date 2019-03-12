b3e9dfb1acba4b6630193897223ff2449d7cdfd8
Bruce Lv <lvkun@outlook.com>
2019-03-13 00:40:20 +0800

    solve isomorphic_strings.

--- src/site/lvkun/leetcode/isomorphic_strings/Solution.java
+++ src/site/lvkun/leetcode/isomorphic_strings/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.leetcode.isomorphic_strings;
 
-public class Solution {
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s != null && t != null && s.length() == t.length()) {
+            int[] mapS = new int[256];
+            int[] mapT = new int[256];
+
+            for (int i = 0; i < s.length(); i++) {
+                char sc = s.charAt(i);
+                char tc = t.charAt(i);
+
+                if (mapS[sc] != mapT[tc]) {
+                    return false;
+                }
+
+                mapS[sc] = i + 1;
+                mapT[tc] = i + 1;
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/site/lvkun/leetcode/isomorphic_strings/SolutionTest.java
+++ src/site/lvkun/leetcode/isomorphic_strings/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.leetcode.isomorphic_strings;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testIsIsomorphic() {
+        Solution solution = new Solution();
+//        Assert.assertTrue(solution.isIsomorphic("egg", "add"));
+//        Assert.assertFalse(solution.isIsomorphic("foo", "bar"));
+//        Assert.assertTrue(solution.isIsomorphic("paper", "title"));
+        Assert.assertFalse(solution.isIsomorphic("ab", "aa"));
+    }
 }
