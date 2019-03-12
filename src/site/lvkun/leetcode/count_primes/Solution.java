abab282b431ef39721a816a2c5530a67a0ce00a7
Bruce Lv <lvkun@outlook.com>
2019-03-13 00:06:39 +0800

    solve count_primes.

--- src/site/lvkun/leetcode/count_primes/Solution.java
+++ src/site/lvkun/leetcode/count_primes/Solution.java
@@ -1,4 +1,21 @@
 package site.lvkun.leetcode.count_primes;
 
-public class Solution {
+import java.util.BitSet;
+
+class Solution {
+    public int countPrimes(int n) {
+        BitSet bitSet = new BitSet();
+
+        int result = 0;
+        for (int i = 2; i < n; i++) {
+            if (!bitSet.get(i)) {
+                result++;
+
+                for (int j = 2; j * i < n; j++) {
+                    bitSet.set(i * j, true);
+                }
+            }
+        }
+        return result;
+    }
 }
--- src/site/lvkun/leetcode/count_primes/SolutionTest.java
+++ src/site/lvkun/leetcode/count_primes/SolutionTest.java
@@ -1,4 +1,12 @@
 package site.lvkun.leetcode.count_primes;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testCountPrimes() {
+        Solution solution = new Solution();
+        Assert.assertEquals(4, solution.countPrimes(10));
+    }
 }
