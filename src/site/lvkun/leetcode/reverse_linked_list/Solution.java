4ba783c5ee6bf96610caddd8de696b2ae4895a32
Bruce Lv <lvkun@outlook.com>
2019-03-14 00:25:06 +0800

    solve reverse_linked_list.

--- src/site/lvkun/leetcode/reverse_linked_list/Solution.java
+++ src/site/lvkun/leetcode/reverse_linked_list/Solution.java
@@ -1,4 +1,27 @@
 package site.lvkun.leetcode.reverse_linked_list;
 
-public class Solution {
+import site.lvkun.leetcode.common.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode cur = head.next;
+        head.next = null;
+        ListNode next = cur.next;
+
+        while (next != null) {
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+            next = next.next;
+        }
+
+        cur.next = prev;
+
+        return cur;
+    }
 }
--- src/site/lvkun/leetcode/reverse_linked_list/SolutionTest.java
+++ src/site/lvkun/leetcode/reverse_linked_list/SolutionTest.java
@@ -1,4 +1,14 @@
 package site.lvkun.leetcode.reverse_linked_list;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.leetcode.common.ListNode;
+
 public class SolutionTest {
+    @Test
+    public void testReverse() {
+        Solution solution = new Solution();
+        ListNode result = solution.reverseList(ListNode.fromString("1->2->3->4->5"));
+        Assert.assertEquals("5->4->3->2->1", result.toString());
+    }
 }
