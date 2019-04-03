618754049288a126b0e4564f145d0893e9166c59
lvkun <lvkun@xiaomi.com>
2019-04-03 18:44:55 +0800

    solve course_schedule_ii.

--- src/site/lvkun/leetcode/course_schedule_ii/Solution.java
+++ src/site/lvkun/leetcode/course_schedule_ii/Solution.java
@@ -1,4 +1,58 @@
 package site.lvkun.leetcode.course_schedule_ii;
 
-public class Solution {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+// TODO: retry
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0 || prerequisites == null) {
+            return null;
+        }
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] in = new int[numCourses];
+        for (int[] edge : prerequisites) {
+            int from = edge[1];
+            int to = edge[0];
+            graph.get(from).add(to);
+            in[to]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        List<Integer> path = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int from = queue.poll();
+            path.add(from);
+
+            List<Integer> adj = graph.get(from);
+
+            for (int to : adj) {
+                in[to]--;
+
+                if (in[to] == 0) {
+                    queue.offer(to);
+                }
+            }
+        }
+
+        for (int i = 0; i < in.length; i++) {
+            in[i] = path.get(i);
+        }
+
+        return in;
+    }
 }
--- src/site/lvkun/leetcode/course_schedule_ii/SolutionTest.java
+++ src/site/lvkun/leetcode/course_schedule_ii/SolutionTest.java
@@ -1,4 +1,20 @@
 package site.lvkun.leetcode.course_schedule_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testFindOrder() {
+        Solution solution = new Solution();
+        int[][] courses = new int[][] {{1,0}};
+        int[] result = solution.findOrder(2, courses);
+        Assert.assertArrayEquals(array(0,1), result);
+
+        courses = new int[][] {{1,0},{2,0},{3,1},{3,2}};
+        result = solution.findOrder(4, courses);
+        Assert.assertArrayEquals(array(0,1,2,3), result);
+    }
 }
