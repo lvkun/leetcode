76a69f60ff3cc9032e8df8374f7c76ad92306335
lvkun <lvkun@xiaomi.com>
2019-04-03 12:38:16 +0800

    solve minimum_size_subarray_sum.

--- src/site/lvkun/leetcode/minimum_size_subarray_sum/Solution.java
+++ src/site/lvkun/leetcode/minimum_size_subarray_sum/Solution.java
@@ -1,4 +1,30 @@
 package site.lvkun.leetcode.minimum_size_subarray_sum;
 
-public class Solution {
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int start = 0;
+        int end = 0;
+
+        int sum = nums[0];
+
+        int minLength = nums.length + 1;
+
+        while (end < nums.length) {
+            while (sum < s && end < nums.length) {
+                sum += nums[end++];
+            }
+
+            while (sum >= s) {
+                minLength = Math.min(minLength, end - start);
+                sum -= nums[start++];
+            }
+        }
+
+        return minLength == nums.length + 1 ? 0 : minLength;
+    }
 }
--- src/site/lvkun/leetcode/minimum_size_subarray_sum/SolutionTest.java
+++ src/site/lvkun/leetcode/minimum_size_subarray_sum/SolutionTest.java
@@ -1,4 +1,14 @@
 package site.lvkun.leetcode.minimum_size_subarray_sum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testMinSubArrayLen() {
+        Solution solution = new Solution();
+        Assert.assertEquals(2, solution.minSubArrayLen(7, array(2,3,1,2,4,3)));
+    }
 }
