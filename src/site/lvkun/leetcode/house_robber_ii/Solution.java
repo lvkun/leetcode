b53fb9c8c8089ed93b1ce5962b69fac7a6a43f10
lvkun <lvkun@xiaomi.com>
2019-04-04 18:31:45 +0800

    solve house_robber_ii.

--- src/site/lvkun/leetcode/house_robber_ii/Solution.java
+++ src/site/lvkun/leetcode/house_robber_ii/Solution.java
@@ -1,4 +1,29 @@
 package site.lvkun.leetcode.house_robber_ii;
 
-public class Solution {
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            sumOdd += nums[i];
+            sumEven += nums[i + 1];
+        }
+
+        int sumOddEnd = sumOdd;
+        if (nums.length % 2 == 1) {
+            sumOddEnd -= nums[0];
+            sumOddEnd += nums[nums.length - 1];
+        }
+
+        return Math.max(sumEven, Math.max(sumOdd, sumOddEnd));
+    }
 }
--- src/site/lvkun/leetcode/house_robber_ii/SolutionTest.java
+++ src/site/lvkun/leetcode/house_robber_ii/SolutionTest.java
@@ -1,4 +1,15 @@
 package site.lvkun.leetcode.house_robber_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testRob() {
+        Solution solution = new Solution();
+//        Assert.assertEquals(3, solution.rob(array(2,3,2)));
+        Assert.assertEquals(4, solution.rob(array(1,2,3,1)));
+    }
 }
