ed680ca88139e513f4ace2690bc5c2c9e87a2399
Bruce Lv <lvkun@outlook.com>
2019-04-05 21:15:19 +0800

    solve kth_largest_element_in_an_array.

--- src/site/lvkun/leetcode/kth_largest_element_in_an_array/Solution.java
+++ src/site/lvkun/leetcode/kth_largest_element_in_an_array/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.leetcode.kth_largest_element_in_an_array;
 
-public class Solution {
+import java.util.PriorityQueue;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length < k) {
+            return 0;
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int n : nums) {
+            if (queue.size() < k) {
+                queue.offer(n);
+            } else {
+                if (n > queue.peek()) {
+                    queue.offer(n);
+                    queue.poll();
+                }
+            }
+        }
+
+        return queue.peek();
+    }
 }
--- src/site/lvkun/leetcode/kth_largest_element_in_an_array/SolutionTest.java
+++ src/site/lvkun/leetcode/kth_largest_element_in_an_array/SolutionTest.java
@@ -1,4 +1,17 @@
 package site.lvkun.leetcode.kth_largest_element_in_an_array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testFindKthLargest() {
+        Solution solution = new Solution();
+        Assert.assertEquals(5,
+                solution.findKthLargest(array(3,2,1,5,6,4), 2));
+        Assert.assertEquals(4,
+                solution.findKthLargest(array(3,2,3,1,2,4,5,5,6), 4));
+    }
 }
