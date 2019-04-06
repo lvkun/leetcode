444770bfb67c35d1f82c9f5e6def091031723121
Bruce Lv <lvkun@outlook.com>
2019-04-06 11:35:48 +0800

    solve contains_duplicate.

--- src/site/lvkun/leetcode/contains_duplicate/Solution.java
+++ src/site/lvkun/leetcode/contains_duplicate/Solution.java
@@ -1,4 +1,23 @@
 package site.lvkun.leetcode.contains_duplicate;
 
-public class Solution {
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            }
+
+            set.add(n);
+        }
+
+        return false;
+    }
 }
--- src/site/lvkun/leetcode/contains_duplicate/SolutionTest.java
+++ src/site/lvkun/leetcode/contains_duplicate/SolutionTest.java
@@ -1,4 +1,16 @@
 package site.lvkun.leetcode.contains_duplicate;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testContainsDuplicate() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.containsDuplicate(array(1,2,3,1)));
+        Assert.assertFalse(solution.containsDuplicate(array(1,2,3,4)));
+        Assert.assertTrue(solution.containsDuplicate(array(1,1,1,3,3,4,3,2,4,2)));
+    }
 }
