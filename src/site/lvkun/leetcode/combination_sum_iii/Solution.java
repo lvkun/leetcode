f1999db0bd056d798cd9bdbc51dc3971aed1da8b
Bruce Lv <lvkun@outlook.com>
2019-04-06 11:11:22 +0800

    solve combination_sum_iii.

--- src/site/lvkun/leetcode/combination_sum_iii/Solution.java
+++ src/site/lvkun/leetcode/combination_sum_iii/Solution.java
@@ -1,4 +1,32 @@
 package site.lvkun.leetcode.combination_sum_iii;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    private void combine(int k, int n, int start, Stack<Integer> result, List<List<Integer>> resultList) {
+        if (n < 0) {
+            return;
+        }
+
+        if (n == 0 && k == result.size()) {
+            resultList.add(new ArrayList<>(result));
+        }
+
+        for (int i = start; i <= 9; i++) {
+            result.push(i);
+            combine(k, n - i, i + 1, result, resultList);
+            result.pop();
+        }
+    }
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Stack<Integer> result = new Stack<>();
+
+        combine(k, n, 1, result, resultList);
+
+        return resultList;
+    }
 }
--- src/site/lvkun/leetcode/combination_sum_iii/SolutionTest.java
+++ src/site/lvkun/leetcode/combination_sum_iii/SolutionTest.java
@@ -1,4 +1,23 @@
 package site.lvkun.leetcode.combination_sum_iii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+    @Test
+    public void testCombinationSum3() {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.combinationSum3(3, 7);
+
+        List<List<Integer>> expected = Arrays.asList(
+            Arrays.asList(1,2,4)
+        );
+
+        Assert.assertEquals(expected, result);
+
+        solution.combinationSum3(3, 9);
+    }
 }
