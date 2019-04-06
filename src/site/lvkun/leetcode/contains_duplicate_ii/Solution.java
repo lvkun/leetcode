077a3ae0d41e5d8e6814622297c1d5300cbf5a98
Bruce Lv <lvkun@outlook.com>
2019-04-06 20:21:15 +0800

    solve contains_duplicate_ii.

--- src/site/lvkun/leetcode/contains_duplicate_ii/Solution.java
+++ src/site/lvkun/leetcode/contains_duplicate_ii/Solution.java
@@ -1,4 +1,19 @@
 package site.lvkun.leetcode.contains_duplicate_ii;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if (map.containsKey(n) && (i - map.get(n)) <= k) {
+                return true;
+            }
+
+            map.put(n, i);
+        }
+        return false;
+    }
 }
--- src/site/lvkun/leetcode/contains_duplicate_ii/SolutionTest.java
+++ src/site/lvkun/leetcode/contains_duplicate_ii/SolutionTest.java
@@ -1,4 +1,16 @@
 package site.lvkun.leetcode.contains_duplicate_ii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testContainsNearbyDuplicate() {
+        Solution solution = new Solution();
+        Assert.assertTrue(solution.containsNearbyDuplicate(array(1,2,3,1), 3));
+        Assert.assertTrue(solution.containsNearbyDuplicate(array(1,0,1,1), 1));
+        Assert.assertFalse(solution.containsNearbyDuplicate(array(1,2,3,1,2,3), 2));
+    }
 }
