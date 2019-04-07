9e1dda9a51029d0740982ee53e67a14f9c0d2c60
Bruce Lv <lvkun@outlook.com>
2019-04-07 11:11:50 +0800

    solve contains_duplicate_iii.

--- src/site/lvkun/leetcode/contains_duplicate_iii/Solution.java
+++ src/site/lvkun/leetcode/contains_duplicate_iii/Solution.java
@@ -1,4 +1,26 @@
 package site.lvkun.leetcode.contains_duplicate_iii;
 
-public class Solution {
+import java.util.Map;
+import java.util.TreeMap;
+
+// TODO: retry
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i - j > k) {
+                map.remove(nums[j]);
+                j++;
+            }
+
+            Map.Entry<Integer, Integer> a = map.ceilingEntry(nums[i] - t);
+            if (a != null && Math.abs(a.getKey() - nums[i]) <= t) {
+                return true;
+            }
+
+            map.put(nums[i], i);
+        }
+        return false;
+    }
 }
--- src/site/lvkun/leetcode/contains_duplicate_iii/SolutionTest.java
+++ src/site/lvkun/leetcode/contains_duplicate_iii/SolutionTest.java
@@ -1,4 +1,22 @@
 package site.lvkun.leetcode.contains_duplicate_iii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static site.lvkun.leetcode.common.ArrayUtils.array;
+
 public class SolutionTest {
+    @Test
+    public void testContainsNearbyAlmostDuplicate() {
+        Solution solution = new Solution();
+//        Assert.assertTrue(solution.containsNearbyAlmostDuplicate(
+//                array(1,2,3,1), 3, 0
+//        ));
+        Assert.assertTrue(solution.containsNearbyAlmostDuplicate(
+                array(1,0,1,1), 1, 2
+        ));
+        Assert.assertFalse(solution.containsNearbyAlmostDuplicate(
+                array(1,5,9,1,5,9), 2, 3
+        ));
+    }
 }
