080d644d6e3dc87458b18cb9a356aef42832d5a3
Bruce Lv <lvkun@outlook.com>
2019-04-08 21:42:20 +0800

    solve maximal_square.

--- src/site/lvkun/leetcode/maximal_square/Solution.java
+++ src/site/lvkun/leetcode/maximal_square/Solution.java
@@ -2,6 +2,26 @@ package site.lvkun.leetcode.maximal_square;
 
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        return 0;
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int res = 0;
+
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] - '0';
+                } else if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+
+        return res * res;
     }
 }
