f3e90d502d73593ce7d3d783b117e8546866f48f
Bruce Lv <lvkun@outlook.com>
2019-04-08 22:47:42 +0800

    solve rectangle_area.

--- src/site/lvkun/leetcode/rectangle_area/Solution.java
+++ src/site/lvkun/leetcode/rectangle_area/Solution.java
@@ -1,4 +1,20 @@
 package site.lvkun.leetcode.rectangle_area;
 
-public class Solution {
+class Solution {
+    private int area(int left, int bottom, int right, int top) {
+        if (right < left || top < bottom) {
+            return 0;
+        }
+
+        return (right - left) * (top - bottom);
+    }
+
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int left = Math.max(A, E);
+        int bottom = Math.max(B, F);
+        int right = Math.min(C, G);
+        int top = Math.min(D, H);
+
+        return area(A, B, C, D)  - area(left, bottom, right, top) + area(E, F, G, H);
+    }
 }
--- src/site/lvkun/leetcode/rectangle_area/SolutionTest.java
+++ src/site/lvkun/leetcode/rectangle_area/SolutionTest.java
@@ -1,4 +1,13 @@
 package site.lvkun.leetcode.rectangle_area;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SolutionTest {
+    @Test
+    public void testComputeArea() {
+        Solution solution = new Solution();
+        int result = solution.computeArea(-3, 0, 3, 4, 0, -1, 9, 2);
+        Assert.assertEquals(45, result);
+    }
 }
