810a5c3fca0a1d0036bab3c1d2ea7b3c17fda9c6
Bruce Lv <lvkun@outlook.com>
2019-04-08 22:34:55 +0800

    solve count_complete_tree_nodes.

--- src/site/lvkun/leetcode/count_complete_tree_nodes/Solution.java
+++ src/site/lvkun/leetcode/count_complete_tree_nodes/Solution.java
@@ -1,4 +1,32 @@
 package site.lvkun.leetcode.count_complete_tree_nodes;
 
-public class Solution {
+import site.lvkun.leetcode.common.TreeNode;
+
+class Solution {
+    private int heightLeft(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return 1 + heightLeft(node.left);
+    }
+
+    private int heightRight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return 1 + heightRight(node.right);
+    }
+
+    public int countNodes(TreeNode root) {
+        int left = heightLeft(root);
+        int right = heightRight(root);
+
+        if (left == right) {
+            return (1 << left) - 1;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
 }
--- src/site/lvkun/leetcode/count_complete_tree_nodes/SolutionTest.java
+++ src/site/lvkun/leetcode/count_complete_tree_nodes/SolutionTest.java
@@ -1,4 +1,20 @@
 package site.lvkun.leetcode.count_complete_tree_nodes;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.leetcode.common.TreeNode;
+
 public class SolutionTest {
+    @Test
+    public void testCountNodes() {
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(3);
+        node.left.left = new TreeNode(4);
+        node.left.right = new TreeNode(5);
+        node.right.left = new TreeNode(6);
+
+        Solution solution = new Solution();
+        Assert.assertEquals(6, solution.countNodes(node));
+    }
 }
