283b3b5f690c717e1623c2fbc144ca858a421220
Bruce Lv <lvkun@outlook.com>
2019-04-11 22:47:44 +0800

    solve invert_binary_tree.

--- src/site/lvkun/leetcode/invert_binary_tree/Solution.java
+++ src/site/lvkun/leetcode/invert_binary_tree/Solution.java
@@ -1,4 +1,17 @@
 package site.lvkun.leetcode.invert_binary_tree;
 
-public class Solution {
+import site.lvkun.leetcode.common.TreeNode;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(temp);
+
+        return root;
+    }
 }
--- src/site/lvkun/leetcode/invert_binary_tree/SolutionTest.java
+++ src/site/lvkun/leetcode/invert_binary_tree/SolutionTest.java
@@ -1,4 +1,31 @@
 package site.lvkun.leetcode.invert_binary_tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+import site.lvkun.leetcode.common.TreeNode;
+
 public class SolutionTest {
+    @Test
+    public void testInvertTree() {
+        TreeNode node = new TreeNode(4);
+        node.left = new TreeNode(2);
+        node.left.left = new TreeNode(1);
+        node.left.right = new TreeNode(3);
+        node.right = new TreeNode(7);
+        node.right.left = new TreeNode(6);
+        node.right.right = new TreeNode(9);
+
+        Solution solution = new Solution();
+        node = solution.invertTree(node);
+
+        TreeNode expected = new TreeNode(4);
+        expected.left = new TreeNode(7);
+        expected.left.left = new TreeNode(9);
+        expected.left.right = new TreeNode(6);
+        expected.right = new TreeNode(2);
+        expected.right.left = new TreeNode(3);
+        expected.right.right = new TreeNode(1);
+
+        Assert.assertEquals(expected, node);
+    }
 }
